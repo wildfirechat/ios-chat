8d5802838cbce82c9b3a186c331c2c8c9cbb6535
heavyrain lee <heavyrain.lee@gmail.com>
2019-03-02 21:09:15 +0800

    no message

---
+++ wfchat/WildFireChat/PCLoginConfirmViewController.h
@@ -0,0 +1,17 @@
+//
+//  PCLoginConfirmViewController.h
+//  WildFireChat
+//
+//  Created by heavyrain lee on 2019/3/2.
+//  Copyright © 2019 WildFireChat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PCLoginConfirmViewController : UIViewController
+@property (nonatomic, strong)NSString *sessionId;
+@end
+
+NS_ASSUME_NONNULL_END
