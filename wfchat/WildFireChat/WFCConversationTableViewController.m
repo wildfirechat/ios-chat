7201fe36fbe3dc919f12ec87a9ae331f3b0d5ca8
heavyrain lee <heavyrain.lee@gmail.com>
2019-03-02 09:10:31 +0800

    no message

--- wfchat/WildFireChat/WFCConversationTableViewController.m
+++ wfchat/WildFireChat/WFCConversationTableViewController.m
@@ -31,16 +31,8 @@
     
     //镜头拉远拉近功能
     vc.isVideoZoom = YES;
+    vc.hidesBottomBarWhenPushed = YES;
     [self.navigationController pushViewController:vc animated:YES];
 }
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
