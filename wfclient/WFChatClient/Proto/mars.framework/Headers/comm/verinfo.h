7863a445d566618fa0971143a6a2031298c1ac3e
heavyrain2012 <heavyrain.lee@gmail.com>
2019-03-28 15:46:09 +0800

    更新协议栈，修改全局屏蔽通知错误

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "ca9040a"
+#define MARS_REVISION "05a04ba"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-03-18 08:14:08"
-#define MARS_TAG "trunk_[2019-03-18_08.14]@git-ca9040a@heavyrain2012"
+#define MARS_BUILD_TIME "2019-03-28 15:39:26"
+#define MARS_TAG "trunk_[2019-03-28_15.39]@git-05a04ba@heavyrain2012"
 
 #endif
