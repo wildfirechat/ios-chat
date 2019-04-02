bef42b4e6866fafc8349dc257796662e5ea19718
heavyrain2012 <heavyrain.lee@gmail.com>
2019-04-02 23:10:01 +0800

    全局静音状态错误

--- wfuikit/WFChatUIKit/ConversationSetting/Cell/WFCUSwitchTableViewCell.m
+++ wfuikit/WFChatUIKit/ConversationSetting/Cell/WFCUSwitchTableViewCell.m
@@ -54,7 +54,7 @@
         }
         case SwitchType_Setting_Global_Silent:
         {
-            [[WFCCIMService sharedWFCIMService] setGlobalSlient:value success:^{
+            [[WFCCIMService sharedWFCIMService] setGlobalSlient:!value success:^{
                
             } error:^(int error_code) {
                
@@ -107,7 +107,7 @@
             value = [[WFCCIMService sharedWFCIMService] getConversationInfo:_conversation].isSilent;
             break;
         case SwitchType_Setting_Global_Silent: {
-            value = [[WFCCIMService sharedWFCIMService] isGlobalSlient];
+            value = ![[WFCCIMService sharedWFCIMService] isGlobalSlient];
             break;
         }
         case SwitchType_Setting_Show_Notification_Detail: {
