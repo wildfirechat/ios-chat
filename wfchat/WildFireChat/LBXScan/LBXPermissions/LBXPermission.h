1a881b06790de22508909999938b29bd8222c985
heavyrain2012 <heavyrain.lee@gmail.com>
2019-04-06 16:49:01 +0800

    苹果上架新政策，用到了healthkit或contact的接口就必须在info.plist中声明。去掉不用的权限
    Former-commit-id: 31d7176014e52c11764c5efacd41ec7242201c2f

--- wfchat/WildFireChat/LBXScan/LBXPermissions/LBXPermission.h
+++ wfchat/WildFireChat/LBXScan/LBXPermissions/LBXPermission.h
@@ -25,26 +25,6 @@ typedef NS_ENUM(NSInteger,LBXPermissionType)
 };
 
 @interface LBXPermission : NSObject
-
-/**
- only effective for location servince,other type reture YES
-
-
- @param type permission type,when type is not location,return YES
- @return YES if system location privacy service enabled NO othersize
- */
-+ (BOOL)isServicesEnabledWithType:(LBXPermissionType)type;
-
-
-/**
- whether device support the type
-
- @param type permission type
- @return  YES if device support
-
- */
-+ (BOOL)isDeviceSupportedWithType:(LBXPermissionType)type;
-
 /**
  whether permission has been obtained, only return status, not request permission
  for example, u can use this method in app setting, show permission status
