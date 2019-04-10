87d7a981fa4a25b3b5aac250fc581c4e10ff9faa
heavyrain2012 <heavyrain.lee@gmail.com>
2019-04-10 22:08:01 +0800

    update lbxscan

--- wfchat/WildFireChat/LBXScan/LBXPermissions/LBXPermission.m
+++ wfchat/WildFireChat/LBXScan/LBXPermissions/LBXPermission.m
@@ -8,39 +8,57 @@
 
 #import "LBXPermission.h"
 #import <UIKit/UIKit.h>
+#import <objc/runtime.h>
+#import <objc/message.h>
 
-#import "LBXPermissionCamera.h"
-#import "LBXPermissionPhotos.h"
+
+typedef void(^completionPermissionHandler)(BOOL granted,BOOL firstTime);
+
 
 @implementation LBXPermission
 
 + (BOOL)authorizedWithType:(LBXPermissionType)type
 {
+    SEL sel = NSSelectorFromString(@"authorized");
+    
+    NSString *strClass = nil;
     switch (type) {
         case LBXPermissionType_Camera:
-            return [LBXPermissionCamera authorized];
+            strClass = @"LBXPermissionCamera";
             break;
         case LBXPermissionType_Photos:
-            return [LBXPermissionPhotos authorized];
+            strClass = @"LBXPermissionPhotos";
             break;
         default:
             break;
     }
+    
+    if (strClass) {
+        BOOL ret  = ((BOOL *(*)(id,SEL))objc_msgSend)( NSClassFromString(strClass), sel);
+        return ret;
+    }
+    
     return NO;
 }
 
 + (void)authorizeWithType:(LBXPermissionType)type completion:(void(^)(BOOL granted,BOOL firstTime))completion
 {
+    NSString *strClass = nil;
     switch (type) {
         case LBXPermissionType_Camera:
-            return [LBXPermissionCamera authorizeWithCompletion:completion];
+            strClass = @"LBXPermissionCamera";
             break;
         case LBXPermissionType_Photos:
-            return [LBXPermissionPhotos authorizeWithCompletion:completion];
+            strClass = @"LBXPermissionPhotos";
             break;
-            
         default:
             break;
+    }
+    
+    if (strClass)
+    {
+        SEL sel = NSSelectorFromString(@"authorizeWithCompletion:");
+        ((void(*)(id,SEL, completionPermissionHandler))objc_msgSend)(NSClassFromString(strClass),sel, completion);
     }
 }
 
