5352ca4b305abd14f0433b25b01aec7a6e026732
heavyrain2012 <heavyrain.lee@gmail.com>
2019-05-18 20:39:37 +0800

    批量获取用户信息，优化联系人列表
    Former-commit-id: 6eaf2ccf53a4ad7ad2b79ececd95c6d2d3340151

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "a21eee7"
+#define MARS_REVISION "e6fc8b7"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-05-11 21:25:49"
-#define MARS_TAG "trunk_[2019-05-11_21.25]@git-a21eee7@tomlee"
+#define MARS_BUILD_TIME "2019-05-18 20:10:02"
+#define MARS_TAG "trunk_[2019-05-18_20.10]@git-e6fc8b7@tomlee"
 
 #endif
