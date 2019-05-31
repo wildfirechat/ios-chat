471db4afb76e39bf8c368d61f11353e4966bfbe1
heavyrain2012 <heavyrain.lee@gmail.com>
2019-05-31 08:40:18 +0800

    首次登录提示修改头像和昵称

--- wfchat/WildFireChat/WFCBaseTabBarController.m
+++ wfchat/WildFireChat/WFCBaseTabBarController.m
@@ -18,11 +18,11 @@
 #define kSelImgKey  @"selectedImageName"
 
 @interface WFCBaseTabBarController ()
-
+@property (nonatomic, strong)UINavigationController *firstNav;
+@property (nonatomic, strong)UINavigationController *settingNav;
 @end
 
 @implementation WFCBaseTabBarController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     
@@ -35,6 +35,8 @@
     item.selectedImage = [[UIImage imageNamed:@"tabbar_chat_cover"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     [item setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor colorWithRed:0.1 green:0.27 blue:0.9 alpha:0.9]} forState:UIControlStateSelected];
     [self addChildViewController:nav];
+    
+    self.firstNav = nav;
     
  
     vc = [WFCUContactListViewController new];
@@ -66,6 +68,21 @@
     item.selectedImage = [[UIImage imageNamed:@"tabbar_me_cover"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     [item setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor colorWithRed:0.1 green:0.27 blue:0.9 alpha:0.9]} forState:UIControlStateSelected];
     [self addChildViewController:nav];
+    self.settingNav = nav;
 }
 
+- (void)setNewUser:(BOOL)newUser {
+    if (newUser) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"欢迎注册" message:@"请更新您头像和昵称，以便您的朋友能更好地识别！" preferredStyle:UIAlertControllerStyleAlert];
+            
+            UIAlertAction *action = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+                self.selectedViewController = self.settingNav;
+            }];
+            [alertController addAction:action];
+            NSLog(@"hahahah");
+            [self.firstNav presentViewController:alertController animated:YES completion:nil];
+        });
+    }
+}
 @end
