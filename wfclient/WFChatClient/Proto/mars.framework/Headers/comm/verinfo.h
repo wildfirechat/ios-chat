adef7485c9f3b648b5e08bdf9900d54652da57f3
heavyrain lee <heavyrain.lee@gmail.com>
2019-06-02 11:18:49 +0800

    1. 更新协议栈 2. 添加对被封禁/token错误/密钥错误的处理。
    Former-commit-id: ba91a7d27c8a4117697890e93759a8e11c4a8b49

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "b2f054e"
+#define MARS_REVISION "386139f"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-05-30 08:09:23"
-#define MARS_TAG "trunk_[2019-05-30_08.09]@git-b2f054e@heavyrainlee"
+#define MARS_BUILD_TIME "2019-06-02 11:03:30"
+#define MARS_TAG "trunk_[2019-06-02_11.03]@git-386139f@heavyrainlee"
 
 #endif
