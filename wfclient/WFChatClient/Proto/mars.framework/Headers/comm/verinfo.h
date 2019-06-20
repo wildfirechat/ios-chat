898c723aa8d1dcc5a1207bc1618ff270d0585611
heavyrain2012 <heavyrain.lee@gmail.com>
2019-06-20 11:54:36 +0800

    更新协议栈，此版本需要服务器版本0.21版本之上才行

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,11 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "a12ff26"
+#define MARS_REVISION "4268c26"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-06-02 11:03:30"
-#define MARS_TAG "trunk_[2019-06-02_11.03]@git-386139f@heavyrainlee"
-#define MARS_BUILD_TIME "2019-06-07 16:32:45"
+#define MARS_BUILD_TIME "2019-06-20 11:47:24"
+#define MARS_TAG "trunk_[2019-06-20_11.47]@git-4268c26@tomlee"
 
 #endif
