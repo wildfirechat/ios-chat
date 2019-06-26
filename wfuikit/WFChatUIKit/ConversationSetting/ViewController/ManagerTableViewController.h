9816b04f8741a379887dfe69c4d9d4f558756a1a
heavyrain2012 <heavyrain.lee@gmail.com>
2019-06-26 09:18:43 +0800

    add group manage code

---
+++ wfuikit/WFChatUIKit/ConversationSetting/ViewController/ManagerTableViewController.h
@@ -0,0 +1,18 @@
+//
+//  ManagerTableViewController.h
+//  WFChatUIKit
+//
+//  Created by heavyrain lee on 2019/6/26.
+//  Copyright © 2019 WildFireChat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <WFChatClient/WFCChatClient.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ManagerTableViewController : UIViewController
+@property (nonatomic, strong)WFCCGroupInfo *groupInfo;
+@end
+
+NS_ASSUME_NONNULL_END
