155e6fa4b32b941de5f56050e312c8825d43275c
heavyrain lee <heavyrain.lee@gmail.com>
2019-07-21 11:56:37 +0800

    Update proto, userId length 48 -> 64
    Former-commit-id: e4471342f34144cc02e6f67193b0b6e2196fef09

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "c45af57"
+#define MARS_REVISION "9cc2a9b"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-07-14 21:09:33"
-#define MARS_TAG "trunk_[2019-07-14_21.09]@git-c45af57@tomlee"
+#define MARS_BUILD_TIME "2019-07-21 11:27:22"
+#define MARS_TAG "trunk_[2019-07-21_11.27]@git-9cc2a9b@heavyrainlee"
 
 #endif
