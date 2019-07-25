891eff53c01406170d716404ae3d080833b5c29e
heavyrain lee <heavyrain.lee@gmail.com>
2019-07-26 07:53:38 +0800

    Update proto to fix getConversationList slow issue

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "9cc2a9b"
+#define MARS_REVISION "434e656"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-07-21 11:27:22"
-#define MARS_TAG "trunk_[2019-07-21_11.27]@git-9cc2a9b@heavyrainlee"
+#define MARS_BUILD_TIME "2019-07-26 07:36:03"
+#define MARS_TAG "trunk_[2019-07-26_07.36]@git-434e656@heavyrainlee"
 
 #endif
