486fd94242c4397bc03a79429f9929071bc05b27
heavyrain.lee <heavyrain.lee@gmail.com>
2019-08-04 18:52:21 +0800

    update

--- wfclient/WFChatClient/Client/WFCCNetworkService.h
+++ wfclient/WFChatClient/Client/WFCCNetworkService.h
@@ -166,8 +166,10 @@ typedef NS_ENUM(NSInteger, ConnectionStatus) {
 
  @param userId 用户Id
  @param token 密码
+ 
+ @return 是否是第一次连接。第一次连接需要同步用户信息，耗时较长，可以加个第一次登录的等待提示界面。
  */
-- (void)connect:(NSString *)userId token:(NSString *)token;
+- (BOOL)connect:(NSString *)userId token:(NSString *)token;
 
 /**
  断开连接
