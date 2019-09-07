8b600c6907ea84738047587e6b7acc08e4faab19
heavyrain.lee <heavyrain.lee@gmail.com>
2019-09-07 12:20:17 +0800

    搜索好友时，同时搜索好友备注

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "9f2a7cae"
+#define MARS_REVISION "cf4d8fc2"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-09-05 22:21:21"
+#define MARS_BUILD_TIME "2019-09-07 12:12:01"
 #define MARS_TAG "iOS_release"
 
 #endif
