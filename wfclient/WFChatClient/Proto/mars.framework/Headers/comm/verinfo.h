527a1bedab9b3d5523f1dd0c0ed891f59de2ec3b
qiuyun <qiuyun@gmail.com>
2019-09-16 13:27:20 +0800

    Revert "Update new proto mars"
    This reverts commit af87315ddf06ec816f5dbccc45c16ffc400564da.

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION ""
-#define MARS_PATH "HEAD"
+#define MARS_REVISION "ad63f530"
+#define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-08-23 18:41:37"
+#define MARS_BUILD_TIME "2019-08-17 16:46:58"
 #define MARS_TAG "iOS_release"
 
 #endif
