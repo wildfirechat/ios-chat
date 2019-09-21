02ba391ac3a5d01d171db90e69690a25df177637
heavyrain2012 <heavyrain.lee@gmail.com>
2019-09-21 18:09:40 +0800

    iOS 13 适配

--- wfuikit/WFChatUIKit/ConversationList/ViewController/WFCUConversationTableViewController.m
+++ wfuikit/WFChatUIKit/ConversationList/ViewController/WFCUConversationTableViewController.m
@@ -52,7 +52,10 @@
     self.searchController.searchResultsUpdater = self;
     self.searchController.delegate = self;
     self.searchController.dimsBackgroundDuringPresentation = NO;
-    [self.searchController.searchBar setValue:WFCString(@"Cancel") forKey:@"_cancelButtonText"];
+    if (! @available(iOS 13, *)) {
+        [self.searchController.searchBar setValue:WFCString(@"Cancel") forKey:@"_cancelButtonText"];
+    }
+
     
     if (@available(iOS 9.1, *)) {
         self.searchController.obscuresBackgroundDuringPresentation = NO;
