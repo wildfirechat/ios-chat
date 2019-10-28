336e3b2bf1b968791f46e835846952fe1e248ac7
heavyrain2012 <heavyrain.lee@gmail.com>
2019-10-28 15:42:53 +0800

    update

--- wfchat/WildFireChat/WFCConfig.m
+++ wfchat/WildFireChat/WFCConfig.m
@@ -8,8 +8,10 @@
 
 #import "WFCConfig.h"
 
-
+//可以是IP，可以是域名，如果是域名的话只支持主域名或www域名，二级域名不支持！
+//例如：example.com或www.example.com是支持的；xx.example.com或xx.yy.example.com是不支持的。
 NSString *IM_SERVER_HOST = @"wildfirechat.cn";
+//最好是80，如果是其他端口，七牛云存储将不被支持
 int IM_SERVER_PORT = 80;
 
 //正式商用时，建议用https，确保token安全
