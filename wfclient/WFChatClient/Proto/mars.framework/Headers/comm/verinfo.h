e8ca7948d92ab6d034631fc3958da5264b7f0981
heavyrain.lee <heavyrain.lee@gmail.com>
2019-11-14 14:12:55 +0800

    更新协议栈解决有时会话时间不正确的问题，另外添加注释

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "9309e895"
+#define MARS_REVISION "c820ecb5"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-11-06 14:56:58"
+#define MARS_BUILD_TIME "2019-11-14 13:42:41"
 #define MARS_TAG "iOS_release"
 
 #endif
