d6ae7620802785f4119f4e3d1469d7640c3f9d4d
heavyrain2012 <heavyrain.lee@gmail.com>
2019-11-27 10:44:00 +0800

    update proto
    Former-commit-id: 1c9f79ae7d552d899d204f85b4507bcc2553989f

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "6d4ec2d3"
+#define MARS_REVISION "cd34407e"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-11-25 10:34:05"
+#define MARS_BUILD_TIME "2019-11-27 10:13:30"
 #define MARS_TAG "iOS_release"
 
 #endif
