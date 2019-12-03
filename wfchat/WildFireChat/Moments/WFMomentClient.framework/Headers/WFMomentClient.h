86dbb71fd85a7c937253792f12118f1afe58f7d1
heavyrain2012 <heavyrain.lee@gmail.com>
2019-12-03 09:08:50 +0800

    添加朋友圈的占位符库
    Former-commit-id: 2ce67d643bda5a34745aa3ba404a1db862529ad7

---
+++ wfchat/WildFireChat/Moments/WFMomentClient.framework/Headers/WFMomentClient.h
@@ -0,0 +1,20 @@
+//
+//  WFMomentClient.h
+//  WFMomentClient
+//
+//  Created by heavyrain.lee on 2019/12/2.
+//  Copyright © 2019 WF Chat. All rights reserved.
+//
+
+// 这个库只是一个占位符，不包含朋友圈的任何代码
+#import <Foundation/Foundation.h>
+
+//! Project version number for WFMomentClient.
+FOUNDATION_EXPORT double WFMomentClientVersionNumber;
+
+//! Project version string for WFMomentClient.
+FOUNDATION_EXPORT const unsigned char WFMomentClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WFMomentClient/PublicHeader.h>
+
+
---
+++ wfchat/WildFireChat/Moments/WFMomentUIKit.framework/Headers/WFMomentUIKit.h
@@ -0,0 +1,20 @@
+//
+//  WFMomentUIKit.h
+//  WFMomentUIKit
+//
+//  Created by heavyrain.lee on 2019/12/2.
+//  Copyright © 2019 WF Chat. All rights reserved.
+//
+
+// 这个库只是一个占位符，不包含朋友圈的任何代码
+#import <Foundation/Foundation.h>
+
+//! Project version number for WFMomentUIKit.
+FOUNDATION_EXPORT double WFMomentUIKitVersionNumber;
+
+//! Project version string for WFMomentUIKit.
+FOUNDATION_EXPORT const unsigned char WFMomentUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WFMomentUIKit/PublicHeader.h>
+
+
