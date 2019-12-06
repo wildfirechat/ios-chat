bb9d6d4d23274b3d57974d986b40b17eaa27dfbd
heavyrain2012 <heavyrain.lee@gmail.com>
2019-12-06 10:17:10 +0800

    update proto

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "d071ff46"
+#define MARS_REVISION "29a8a63d"
 #define MARS_PATH "HEAD"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-12-05 20:46:10"
+#define MARS_BUILD_TIME "2019-12-06 10:05:02"
 #define MARS_TAG "iOS_release"
 
 #endif
