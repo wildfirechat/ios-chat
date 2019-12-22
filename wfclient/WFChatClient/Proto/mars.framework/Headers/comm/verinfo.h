8c1017174b5babb07fddd4a83f40d3257d868282
heavyrain2012 <heavyrain.lee@gmail.com>
2019-12-22 17:48:24 +0800

    update version

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "ec70da9f"
+#define MARS_REVISION "27a49a0b"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-12-21 13:02:32"
+#define MARS_BUILD_TIME "2019-12-22 17:37:51"
 #define MARS_TAG ""
 
 #endif
