225b5c8fe9fa7e3b6ec4f53d9655612d231228ad
heavyrain.lee <heavyrain.lee@gmail.com>
2020-01-13 17:42:42 +0800

    Revert "update proto"
    This reverts commit e75c3f02b2f20454fdcc3ad9b1b99676e427ef10.

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "30e41cb9"
+#define MARS_REVISION "01e76097"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-01-13 17:10:13"
+#define MARS_BUILD_TIME "2020-01-11 21:11:35"
 #define MARS_TAG ""
 
 #endif
