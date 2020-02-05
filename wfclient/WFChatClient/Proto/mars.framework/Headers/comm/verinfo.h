ba71960447e33aad906cc6176541306321d87bca
heavyrain2012 <heavyrain.lee@gmail.com>
2020-02-05 15:14:15 +0800

    update

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "30e41cb9"
+#define MARS_REVISION "14c36c7c"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-01-13 17:51:59"
+#define MARS_BUILD_TIME "2020-02-05 15:04:42"
 #define MARS_TAG ""
 
 #endif
