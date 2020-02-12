c23377831b5e884cfe6db1b05661a195946a2112
heavyrain2012 <heavyrain.lee@gmail.com>
2020-02-12 08:33:34 +0800

    update proto to fix upload meida error

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "055ee36e"
+#define MARS_REVISION "934776bd"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-02-06 11:41:26"
+#define MARS_BUILD_TIME "2020-02-12 08:25:15"
 #define MARS_TAG ""
 
 #endif
