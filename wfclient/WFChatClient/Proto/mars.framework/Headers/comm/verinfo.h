a6dff9062ecf97ae20e1ac207df944691c9262ef
heavyrain2012 <heavyrain.lee@gmail.com>
2020-03-23 15:24:04 +0800

    解决语音消息一播放状态错误问题
    Former-commit-id: 6127d5c2dfd2c7bcf51df6b3bdfa9cfdf580bf18

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "8b6710d3"
+#define MARS_REVISION "1a38d305"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-03-18 14:54:56"
+#define MARS_BUILD_TIME "2020-03-23 15:16:06"
 #define MARS_TAG ""
 
 #endif
