c132f1a893037002864c284daeaae4b96fe08ae9
heavyrain.lee <heavyrain.lee@gmail.com>
2020-03-23 10:16:29 +0800

    voip ui

--- wfuikit/WFChatUIKit/MessageList/Cell/WFCUCallSummaryCell.m
+++ wfuikit/WFChatUIKit/MessageList/Cell/WFCUCallSummaryCell.m
@@ -22,26 +22,83 @@
 #define TEXT_LABEL_LEFT_PADDING 30
 #define TEXT_LABEL_RIGHT_PADDING 30
 
+#import <WFAVEngineKit/WFAVEngineKit.h>
+
 @implementation WFCUCallSummaryCell
 
 + (CGSize)sizeForClientArea:(WFCUMessageModel *)msgModel withViewWidth:(CGFloat)width {
-    return CGSizeMake(98, 30);
+    NSString *text = [WFCUCallSummaryCell getCallText:msgModel.message.content];
+    CGSize textSize = [WFCUUtilities getTextDrawingSize:text font:[UIFont systemFontOfSize:18] constrainedSize:CGSizeMake(width, 8000)];
+    return CGSizeMake(textSize.width + 25, 30);
+}
+
++ (NSString *)getCallText:(WFCCCallStartMessageContent *)startContent {
+    NSString *text = WFCString(@"VOIPCall");
+    if (startContent.connectTime > 0 && startContent.endTime > 0) {
+        long long duration = startContent.endTime - startContent.connectTime;
+        if (duration <= 0) {
+            return text;
+        }
+        duration = duration/1000; //转化成s
+        if (duration == 0) {
+            return text;
+        }
+        
+        long long hour = duration/3600; //小时数
+        duration = duration - hour * 3600; //去除小时
+        long long mins = duration/60;  //分钟数
+        duration = duration - mins*60;
+        long long second = duration;
+        
+        if (hour) {
+            text = [text stringByAppendingFormat:@"%lld:", hour];
+        }
+        
+        text = [text stringByAppendingFormat:@"%02lld:", mins];
+        text = [text stringByAppendingFormat:@"%02lld", second];
+    } else {
+        switch (startContent.status) {
+            case kWFAVCallEndReasonAcceptByOtherClient:
+                text = @"其它端已接听";
+                break;
+            case kWFAVCallEndReasonBusy:
+            case kWFAVCallEndReasonRemoteBusy:
+                text = @"线路忙";
+                break;
+            case kWFAVCallEndReasonRemoteTimeout:
+                text = @"对方未接听";
+                break;
+            case kWFAVCallEndReasonRemoteHangup:
+                text = @"对方已拒绝";
+                break;
+            case kWFAVCallEndReasonRemoteNetworkError:
+                text = @"网络错误";
+                break;
+                
+            default:
+                break;
+        }
+    }
+    
+    return text;
 }
 
 - (void)setModel:(WFCUMessageModel *)model {
     [super setModel:model];
-
+    
     CGFloat width = self.contentArea.bounds.size.width;
     
-    self.infoLabel.text = WFCString(@"VOIPCall");
+    self.infoLabel.text = [WFCUCallSummaryCell getCallText:model.message.content];
     self.infoLabel.layoutMargins = UIEdgeInsetsMake(TEXT_TOP_PADDING, TEXT_LEFT_PADDING, TEXT_BUTTOM_PADDING, TEXT_RIGHT_PADDING);
     
+    CGSize textSize = [WFCUUtilities getTextDrawingSize:self.infoLabel.text font:[UIFont systemFontOfSize:18] constrainedSize:CGSizeMake(width, 8000)];
+    
     if (model.message.direction == MessageDirection_Send) {
-        self.infoLabel.frame = CGRectMake(width - 98, 0, 70, 30);
+        self.infoLabel.frame = CGRectMake(0, 0, width - 25, 30);
         self.modeImageView.frame = CGRectMake(width - 25, 3, 25, 25);
     } else {
-        self.infoLabel.frame = CGRectMake(0, 0, 70, 30);
-        self.modeImageView.frame = CGRectMake(70, 3, 25, 25);
+        self.infoLabel.frame = CGRectMake(0, 0, width-25, 30);
+        self.modeImageView.frame = CGRectMake(width-25, 3, 25, 25);
     }
     if ([self.model.message.content isKindOfClass:[WFCCCallStartMessageContent class]]) {
         WFCCCallStartMessageContent *startContent = (WFCCCallStartMessageContent *)self.model.message.content;
