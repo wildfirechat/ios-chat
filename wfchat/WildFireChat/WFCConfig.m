d0121108623a2a65ea8bc3c8ab22788bb4975921
heavyrain2012 <heavyrain.lee@gmail.com>
2020-03-30 21:53:11 +0800

    udpate

--- wfchat/WildFireChat/WFCConfig.m
+++ wfchat/WildFireChat/WFCConfig.m
@@ -12,12 +12,10 @@
 //例如：example.com或www.example.com或im.example.com或imtest.example.com是支持的；xx.example.com或xx.yy.example.com是不支持的。如果是专业版必须用域名。
 NSString *IM_SERVER_HOST = @"wildfirechat.cn";
 
-//客户端强制使用80端口，不能使用其它端口。需要确保服务器运行在在国内时处于备案状态，确保运营IM服务处在监管之下。
-//int IM_SERVER_PORT = 80;
 
 //正式商用时，建议用https，确保token安全
 NSString *APP_SERVER_ADDRESS = @"http://wildfirechat.cn:8888";
-//NSString *APP_SERVER_ADDRESS = @"https://wildfirechat.cn/app";
+//NSString *APP_SERVER_ADDRESS = @"https://app.wildfirechat.cn";
 
 NSString *ICE_ADDRESS = @"turn:turn.wildfirechat.cn:3478";
 NSString *ICE_USERNAME = @"wfchat";
