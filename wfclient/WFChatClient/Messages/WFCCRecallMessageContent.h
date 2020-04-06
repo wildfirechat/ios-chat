547748cb597dfa8d4b4e6f8c8d09efa5103b153d
heavyrain.lee <heavyrain.lee@gmail.com>
2020-04-06 10:34:25 +0800

    撤回消息加上被撤回信息

--- wfclient/WFChatClient/Messages/WFCCRecallMessageContent.h
+++ wfclient/WFChatClient/Messages/WFCCRecallMessageContent.h
@@ -22,4 +22,34 @@
  撤回用户Id
  */
 @property (nonatomic, strong)NSString *operatorId;
+
+/**
+被撤回消息的发送者用户Id
+*/
+@property (nonatomic, strong)NSString *originalSender;
+
+/**
+撤回消息的内容类型
+*/
+@property (nonatomic, assign)int originalContentType;
+
+/**
+撤回消息的可搜索内容类型
+*/
+@property (nonatomic, strong)NSString *originalSearchableContent;
+
+/**
+撤回消息的内容
+*/
+@property (nonatomic, strong)NSString *originalContent;
+
+/**
+撤回消息的Extra内容
+*/
+@property (nonatomic, strong)NSString *originalExtra;
+
+/**
+撤回消息的时间戳
+*/
+@property (nonatomic, assign)long long originalMessageTimestamp;
 @end
