d287a998f4b45d0730d735b66ce59374904ff89f
heavyrain2012 <heavyrain.lee@gmail.com>
2020-04-08 14:24:24 +0800

    update proto
    Former-commit-id: a8d007706003f745d3429767c2b7be65ce8a52af

--- wfclient/WFChatClient/Proto/mars.framework/Headers/app/app.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/app/app.h
@@ -41,7 +41,10 @@ struct AccountInfo {
         PlatformType_Android = 2,
         PlatformType_Windows = 3,
         PlatformType_OSX = 4,
-        PlatformType_WEB = 5
+        PlatformType_WEB = 5,
+        Platform_WX = 6,
+        Platform_Linux = 7,
+        Platform_iPad = 8
     } PlatformType;
     
 struct DeviceInfo {
