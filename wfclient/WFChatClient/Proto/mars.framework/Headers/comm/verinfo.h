f1056d9069d6f6e16e5cd3e3816a394660b8af1c
heavyrain.lee <heavyrain.lee@gmail.com>
2020-04-23 12:25:04 +0800

    fix proto error
    Former-commit-id: 00d00da4cc50b9dfa91f869f45438e514fdbf0c6

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "f7dde11c"
+#define MARS_REVISION "e2ba61b9"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-04-22 13:46:23"
+#define MARS_BUILD_TIME "2020-04-23 12:18:28"
 #define MARS_TAG ""
 
 #endif
