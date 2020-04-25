8915ca5efd02954d5146349986c03586a07b2c1a
heavyrain.lee <heavyrain.lee@gmail.com>
2020-04-25 11:09:08 +0800

    fix build error
    Former-commit-id: 1a66c719b63f97f94d204d6adeb7a22674cb2119

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "e2ba61b9"
+#define MARS_REVISION "f8dfc7f4"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-04-23 12:18:28"
+#define MARS_BUILD_TIME "2020-04-25 11:03:14"
 #define MARS_TAG ""
 
 #endif
