0c5339fd496fc7a23c274c31f8b0546caec63462
heavyrain2012 <heavyrain.lee@gmail.com>
2020-06-09 20:39:54 +0800

    修正解散群组的问题
    Former-commit-id: 7675b235dfa0093ac5ec0cb11c5497f5b6bfd02c

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "e7338095"
+#define MARS_REVISION "f8e91e6c"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-06-06 22:56:01"
+#define MARS_BUILD_TIME "2020-06-09 20:31:07"
 #define MARS_TAG ""
 
 #endif
