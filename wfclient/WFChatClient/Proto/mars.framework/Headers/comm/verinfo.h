59f691411b150ccf2213b03b251c6a552d9f992e
heavyrain.lee <heavyrain.lee@gmail.com>
2020-06-22 17:15:03 +0800

    update proto to fix recall issue
    Former-commit-id: 3a8b670fa3ae8f6c1b2bcead811eef2f1bf631ff

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -5,7 +5,7 @@
 #define MARS_REVISION "529efd71"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-06-11 18:15:27"
+#define MARS_BUILD_TIME "2020-06-22 16:56:27"
 #define MARS_TAG ""
 
 #endif
