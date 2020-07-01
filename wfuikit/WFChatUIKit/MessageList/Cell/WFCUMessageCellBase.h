98164154b94c9d01fa73a402abeca52aeaa5e979
heavyrain2012 <heavyrain.lee@gmail.com>
2020-07-01 21:02:11 +0800

    查看文本消息

--- wfuikit/WFChatUIKit/MessageList/Cell/WFCUMessageCellBase.h
+++ wfuikit/WFChatUIKit/MessageList/Cell/WFCUMessageCellBase.h
@@ -24,6 +24,7 @@
 
 @optional
 - (void)didTapReceiptView:(WFCUMessageCellBase *)cell withModel:(WFCUMessageModel *)model;
+- (void)didDoubleTapMessageCell:(WFCUMessageCellBase *)cell withModel:(WFCUMessageModel *)model;
 @end
 
 @interface WFCUMessageCellBase : UICollectionViewCell
