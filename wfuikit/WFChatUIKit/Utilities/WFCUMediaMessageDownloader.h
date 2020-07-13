51526ede06dfcd4f6ede148b83a8909985729276
heavyrain2012 <heavyrain.lee@gmail.com>
2020-07-13 17:15:06 +0800

    update

--- wfuikit/WFChatUIKit/Utilities/WFCUMediaMessageDownloader.h
+++ wfuikit/WFChatUIKit/Utilities/WFCUMediaMessageDownloader.h
@@ -30,4 +30,14 @@ typedef NS_ENUM(NSInteger, DownloadMediaType) {
 - (BOOL)tryDownload:(WFCCMessage *)msg
             success:(void(^)(long long messageUid, NSString *localPath))successBlock
               error:(void(^)(long long messageUid, int error_code))errorBlock;
+
+
+/*
+* @return YES 正在下载； NO 无法下载。朋友圈会使用这个方法
+*/
+- (BOOL)tryDownload:(NSString *)mediaPath
+                uid:(long long)uid
+          mediaType:(DownloadMediaType)mediaType
+            success:(void(^)(long long messageUid, NSString *localPath))successBlock
+              error:(void(^)(long long messageUid, int error_code))errorBlock;
 @end
