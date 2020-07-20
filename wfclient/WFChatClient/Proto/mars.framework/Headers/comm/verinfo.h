430939656d3eff330bb686f52d002eac7839cf67
heavyrain2012 <heavyrain.lee@gmail.com>
2020-07-20 15:20:54 +0800

    update  proto to fix crash issue
    Former-commit-id: 9248bad2767facc45659930dc09c4fa824b791ee

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "99c1b7ae"
+#define MARS_REVISION "2071781d"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-07-16 16:54:46"
+#define MARS_BUILD_TIME "2020-07-20 15:09:02"
 #define MARS_TAG ""
 
 #endif
