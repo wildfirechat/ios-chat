9922c657e320cac4ee3dd4d186d9eaf8477793bc
heavyrain2012 <heavyrain.lee@gmail.com>
2020-08-12 16:20:57 +0800

    修正在群组中收到添加群成员时，当本地没有新增成员用户信息时显示id的问题

--- wfuikit/WFChatUIKit/MessageList/Cell/WFCUInformationCell.m
+++ wfuikit/WFChatUIKit/MessageList/Cell/WFCUInformationCell.m
@@ -44,6 +44,28 @@
 - (void)setModel:(WFCUMessageModel *)model {
     [super setModel:model];
     
+    __weak typeof(self)ws = self;
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [[NSNotificationCenter defaultCenter] addObserverForName:kUserInfoUpdated object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
+        WFCCUserInfo *userInfo = note.userInfo[@"userInfo"];
+        BOOL needUpdate = false;
+        if ([ws.model.message.content isKindOfClass:[WFCCAddGroupeMemberNotificationContent class]]) {
+            WFCCAddGroupeMemberNotificationContent *cnt = (WFCCAddGroupeMemberNotificationContent *)ws.model.message.content;
+            if ([cnt.invitor isEqualToString:userInfo.userId] || [cnt.invitees containsObject:userInfo.userId]) {
+                needUpdate = true;
+            }
+        } else if ([ws.model.message.content isKindOfClass:[WFCCCreateGroupNotificationContent class]]) {
+            WFCCCreateGroupNotificationContent *cnt = (WFCCCreateGroupNotificationContent *)ws.model.message.content;
+            if ([cnt.creator isEqualToString:userInfo.userId]) {
+                needUpdate = true;
+            }
+        }
+        
+        if (needUpdate) {
+            [ws setModel:ws.model];
+        }
+    }];
+    
     NSString *infoText;
     if ([model.message.content isKindOfClass:[WFCCNotificationMessageContent class]]) {
         WFCCNotificationMessageContent *content = (WFCCNotificationMessageContent *)model.message.content;
@@ -87,5 +109,9 @@
         [self.contentView addSubview:_infoLabel];
     }
     return _infoLabel; 
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 @end
