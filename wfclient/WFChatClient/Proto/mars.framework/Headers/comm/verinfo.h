514c1566d7026a90eba5e0267b7411936b290ebd
heavyrain2012 <heavyrain.lee@gmail.com>
2020-09-02 12:44:26 +0800

    更新协议栈，优化退出重新登录的场景
    Former-commit-id: 7d541d6bf85dd79dd655384384207c8b27f6e960

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "0d646c72"
+#define MARS_REVISION "d61d6add"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-09-01 11:10:28"
+#define MARS_BUILD_TIME "2020-09-02 10:39:50"
 #define MARS_TAG ""
 
 #endif
