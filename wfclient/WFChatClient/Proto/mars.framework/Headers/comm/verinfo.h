5d9ebe86298e1561b73bd8371eef8550b8b8205b
heavyrain2012 <heavyrain.lee@gmail.com>
2020-09-03 16:03:51 +0800

    update proto to fix friend request disapper issue

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "d61d6add"
+#define MARS_REVISION "5514bf57"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-09-02 10:39:50"
+#define MARS_BUILD_TIME "2020-09-03 15:56:51"
 #define MARS_TAG ""
 
 #endif
