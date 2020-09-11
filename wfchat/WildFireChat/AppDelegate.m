557157e46d539cfa92bbb5ec76a254e13a022784
heavyrain2012 <heavyrain.lee@gmail.com>
2020-09-11 12:20:52 +0800

    注释掉修改音视频电话最大参与人数的限制，使用默认值

--- wfchat/WildFireChat/AppDelegate.m
+++ wfchat/WildFireChat/AppDelegate.m
@@ -56,9 +56,12 @@
     [[WFAVEngineKit sharedEngineKit] addIceServer:ICE_ADDRESS userName:ICE_USERNAME password:ICE_PASSWORD];
     [[WFAVEngineKit sharedEngineKit] setVideoProfile:kWFAVVideoProfile360P swapWidthHeight:YES];
     [WFAVEngineKit sharedEngineKit].delegate = self;
-    [WFAVEngineKit sharedEngineKit].maxVideoCallCount = 4;
-    [WFAVEngineKit sharedEngineKit].maxAudioCallCount = 9;
     
+    // 设置音视频参与者数量。多人音视频默认视频4路，音频9路，如果改成更多可能会导致问题；音视频高级版默认视频9路，音频16路。
+//    [WFAVEngineKit sharedEngineKit].maxVideoCallCount = 4;
+//    [WFAVEngineKit sharedEngineKit].maxAudioCallCount = 9;
+    
+    //音视频日志，当需要抓日志分析时可以打开这句话
     //RTCSetMinDebugLogLevel(RTCLoggingSeverityInfo);
 #endif
     
