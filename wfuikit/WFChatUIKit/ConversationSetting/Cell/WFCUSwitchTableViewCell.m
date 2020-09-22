b1df1a0ad22153122b68300ee220745f53dfcfa1
heavyrain2012 <heavyrain.lee@gmail.com>
2020-09-22 21:19:05 +0800

    修正ios14系统上，部分按钮不可用问题

--- wfuikit/WFChatUIKit/ConversationSetting/Cell/WFCUSwitchTableViewCell.m
+++ wfuikit/WFChatUIKit/ConversationSetting/Cell/WFCUSwitchTableViewCell.m
@@ -26,7 +26,7 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if(self) {
         self.valueSwitch = [[UISwitch alloc] initWithFrame:CGRectMake([UIScreen mainScreen].bounds.size.width - 56, 8, 40, 40)];
-        [self addSubview:self.valueSwitch];
+        [self.contentView addSubview:self.valueSwitch];
         [self.valueSwitch addTarget:self action:@selector(onSwitch:) forControlEvents:UIControlEventValueChanged];
         self.type = SwitchType_Conversation_None;
         self.conversation = conversation;
