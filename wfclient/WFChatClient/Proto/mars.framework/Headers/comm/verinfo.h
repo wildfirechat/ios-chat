80ebefdc7bd864a42c78d2a2e68c43a7ba537bc7
imndx <imndx@gmail.com>
2020-09-24 15:29:56 +0800

    update proto
    Former-commit-id: 124bc214a0a91eaa64f3d219c6d3b80405345bb2

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "583ed7fc"
+#define MARS_REVISION "e250b953"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-09-24 10:30:30"
+#define MARS_BUILD_TIME "2020-09-24 15:21:22"
 #define MARS_TAG ""
 
 #endif
