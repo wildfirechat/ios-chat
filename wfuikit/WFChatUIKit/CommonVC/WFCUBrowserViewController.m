07d3f3ee25682f35c193b02a8bde4b91987d1993
heavyrain2012 <heavyrain.lee@gmail.com>
2020-10-13 15:15:02 +0800

    浏览器添加转发给朋友的选项

--- wfuikit/WFChatUIKit/CommonVC/WFCUBrowserViewController.m
+++ wfuikit/WFChatUIKit/CommonVC/WFCUBrowserViewController.m
@@ -7,6 +7,7 @@
 //
 #import <WebKit/WebKit.h>
 #import "WFCUBrowserViewController.h"
+#import "WFCUForwardViewController.h"
 
 @interface WFCUBrowserViewController ()
 @property (nonatomic, strong)WKWebView *webView;
@@ -18,14 +19,51 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds];
+    
     [self.view addSubview:self.webView];
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[self.url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]]]];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"..." style:UIBarButtonItemStyleDone target:self action:@selector(onRightBtn:)];
 }
 
 - (void)onRightBtn:(id)sender {
-    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:WFCString(@"Cancel") destructiveButtonTitle:WFCString(@"OpenInBrowser") otherButtonTitles:nil, nil];
-    [actionSheet showInView:self.view];
+    UIAlertController* alertController = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    __weak typeof(self)ws = self;
+    // Create cancel action.
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:WFCString(@"Cancel") style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+        
+    }];
+    [alertController addAction:cancelAction];
+    
+    UIAlertAction *openInBrowserAction = [UIAlertAction actionWithTitle:WFCString(@"OpenInBrowser") style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [[UIApplication sharedApplication] openURL:[[NSURL alloc] initWithString:[ws.url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]]];
+        [ws.navigationController popViewControllerAnimated:NO];
+    }];
+    [alertController addAction:openInBrowserAction];
+    
+    UIAlertAction *sendToFriendAction = [UIAlertAction actionWithTitle:WFCString(@"SendToFriend") style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        WFCUForwardViewController *controller = [[WFCUForwardViewController alloc] init];
+        WFCCLinkMessageContent *link = [[WFCCLinkMessageContent alloc] init];
+        link.title = ws.webView.title;
+        link.url = ws.webView.URL.absoluteString;
+        link.thumbnailUrl = [NSString stringWithFormat:@"%@://%@/favicon.ico", ws.webView.URL.scheme, ws.webView.URL.host];
+        WFCCMessage *msg = [[WFCCMessage alloc] init];
+        msg.content = link;
+        
+        controller.message = msg;
+        UINavigationController *navi = [[UINavigationController alloc] initWithRootViewController:controller];
+        [ws.navigationController presentViewController:navi animated:YES completion:nil];
+    }];
+    [alertController addAction:sendToFriendAction];
+    
+    if(NSClassFromString(@"SDTimeLineTableViewController")) {
+        UIAlertAction *sendToMomentsAction = [UIAlertAction actionWithTitle:WFCString(@"SendToMoments") style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+            
+        }];
+        [alertController addAction:sendToMomentsAction];
+    }
+    
+    [self.navigationController presentViewController:alertController animated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,21 +71,4 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
-#pragma mark -  UIActionSheetDelegate <NSObject>
-- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
-    if(buttonIndex == 0) {
-        [[UIApplication sharedApplication] openURL:[[NSURL alloc] initWithString:[self.url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]]];
-        [self.navigationController popViewControllerAnimated:NO];
-    }
-}
 @end
