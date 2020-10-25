87939398006a6399e569e67b3062d9fb9e0c74b9
heavyrain2012 <heavyrain.lee@gmail.com>
2020-10-25 14:43:11 +0800

    fix build error

--- wfuikit/xcodescript.sh
+++ wfuikit/xcodescript.sh
@@ -23,3 +23,4 @@ lipo -create $BIN_DIR_TMP/*-${TARGET_NAME}.framework/${TARGET_NAME} -output ${BI
 cp -af ${BIN_DIR}/* ${DST_DIR}/
 cp -af WFChatUIKit/Resources/*  ${DST_DIR}/
 cp -af WFChatUIKit/AVEngine/*  ${DST_DIR}/
+cp -af WFChatUIKit/Vendor/ZLPhotoBrowser/*  ${DST_DIR}/
