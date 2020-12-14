9a0383815e1c71f06e811eca051f814eb1243e7a
imhao183 <wildfirechat86@gmail.com>
2020-12-14 09:45:04 +0800

    1，添加按照时间获取消息的接口 2，当会话设置免打扰和置顶后，删除消息再加载会话，需要正确返沪免打扰和置顶状态

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "e52b4697"
+#define MARS_REVISION "5ac8c444"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-11-12 15:38:46"
+#define MARS_BUILD_TIME "2020-12-14 09:30:41"
 #define MARS_TAG ""
 
 #endif
