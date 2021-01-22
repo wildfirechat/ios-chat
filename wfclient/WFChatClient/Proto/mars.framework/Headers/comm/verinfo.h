c4751cf2d891a13d737e6e6036808193db971ead
imhao183 <wildfirechat86@gmail.com>
2021-01-22 09:50:16 +0800

    update proto

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "ce39bd9e"
+#define MARS_REVISION "7ca47622"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-01-14 20:49:22"
+#define MARS_BUILD_TIME "2021-01-22 09:39:41"
 #define MARS_TAG ""
 
 #endif
