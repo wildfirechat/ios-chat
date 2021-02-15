2cafbd1499c1e93ef4eb4f42cb60f566ca6be92a
heavyrain2012 <heavyrain.lee@gmail.com>
2021-02-15 23:10:50 +0800

    会议相关UI代码

---
+++ wfuikit/WFChatUIKit/Voip/Conference/Message/WFCUConferenceChangeModelContent.m
@@ -0,0 +1,51 @@
+//
+//  WFCUConferenceChangeModelContent.m
+//  WFChatUIKit
+//
+//  Created by Tom Lee on 2021/2/15.
+//  Copyright © 2021 Tom Lee. All rights reserved.
+//
+
+#import "WFCUConferenceChangeModelContent.h"
+
+@implementation WFCUConferenceChangeModelContent
+-(WFCCMessagePayload *)encode {
+    WFCCMessagePayload *payload = [[WFCCMessagePayload alloc] init];
+    payload.contentType = [self.class getContentType];
+    payload.content = self.conferenceId;
+    
+    NSMutableDictionary *dataDict = [NSMutableDictionary dictionary];
+    if (self.isAudience) {
+        [dataDict setObject:@(YES) forKey:@"a"];
+    }
+    
+    payload.binaryContent = [NSJSONSerialization dataWithJSONObject:dataDict
+                                                            options:kNilOptions
+                                                              error:nil];
+    
+    return payload;
+}
+-(void)decode:(WFCCMessagePayload *)payload {
+    self.conferenceId = payload.content;
+    
+    NSError *__error = nil;
+    NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:payload.binaryContent
+                                                               options:kNilOptions
+                                                                 error:&__error];
+    if (!__error) {
+        self.isAudience = [dictionary[@"a"] boolValue];
+    }
+}
+
++ (int)getContentType {
+    return VOIP_CONTENT_CONFERENCE_CHANGE_MODE;
+}
+
++ (int)getContentFlags {
+    return WFCCPersistFlag_TRANSPARENT;
+}
+
++ (void)load {
+    [[WFCCIMService sharedWFCIMService] registerMessageContent:self];
+}
+@end
