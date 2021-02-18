8951bf7979c3c0081ad39222bcdb31be780a93fa
heavyrain2012 <heavyrain.lee@gmail.com>
2021-02-18 15:57:25 +0800

    切换到net域名

--- wfchat/WildFireChat/WFCConfig.m
+++ wfchat/WildFireChat/WFCConfig.m
@@ -10,7 +10,7 @@
 
 //可以是IP，可以是域名，如果是域名的话只支持主域名或www域名或im或imtest的二级域名，其它二级域名不支持！
 //例如：example.com或www.example.com或im.example.com或imtest.example.com是支持的；xx.example.com或xx.yy.example.com是不支持的。如果是专业版必须用域名，社区版建议也用域名。
-NSString *IM_SERVER_HOST = @"wildfirechat.cn";
+NSString *IM_SERVER_HOST = @"wildfirechat.net";
 
 
 // App Server默认使用的是8888端口，替换为自己部署的服务时需要注意端口别填错了，使用http访问appserver时，需要确保appserver的配置文件中的wfc.all_client_support_ssl参数为false
@@ -18,10 +18,10 @@ NSString *IM_SERVER_HOST = @"wildfirechat.cn";
 // 如果您使用web-chat，由于最新chrome浏览器的策略，只有使用https才能带上cookie访问appserver的接口，所以就必须使
 // wfc.all_client_support_ssl为tue，所以客户端也必须使用https的应用服务地址
 
-//NSString *APP_SERVER_ADDRESS = @"http://wildfirechat.cn:8888";
-NSString *APP_SERVER_ADDRESS = @"https://app.wildfirechat.cn";
+//NSString *APP_SERVER_ADDRESS = @"http://wildfirechat.net:8888";
+NSString *APP_SERVER_ADDRESS = @"https://app.wildfirechat.net";
 
-NSString *ICE_ADDRESS = @"turn:turn.wildfirechat.cn:3478";
+NSString *ICE_ADDRESS = @"turn:turn.wildfirechat.net:3478";
 NSString *ICE_USERNAME = @"wfchat";
 NSString *ICE_PASSWORD = @"wfchat";
 
