97e77175394a96bec0ed9344fe75deae648204e4
imhao183 <wildfirechat86@gmail.com>
2021-02-18 21:58:49 +0800

    修正ptt错误
    Former-commit-id: 6064137a93975023e0735f5b98b7bca755d55266

--- wfuikit/WFChatUIKit/PTT/WFCUPushToTalkMemberListViewController.m
+++ wfuikit/WFChatUIKit/PTT/WFCUPushToTalkMemberListViewController.m
@@ -9,9 +9,12 @@
 #import "WFCUPushToTalkMemberListViewController.h"
 #import <WFAVEngineKit/WFAVEngineKit.h>
 #import "WFCUConferenceInviteViewController.h"
+#import "WFCUConferenceMember.h"
+#import "WFCUConferenceMemberTableViewCell.h"
 
-@interface WFCUPushToTalkMemberListViewController ()
-
+@interface WFCUPushToTalkMemberListViewController () <UITableViewDataSource, UITableViewDelegate>
+@property(nonatomic, strong)UITableView *tableView;
+@property (nonatomic, strong) NSMutableArray<WFCUConferenceMember *> *participants;
 @end
 
 @implementation WFCUPushToTalkMemberListViewController
@@ -20,6 +23,47 @@
     [super viewDidLoad];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"关闭" style:UIBarButtonItemStyleDone target:self action:@selector(onClose:)];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"邀请" style:UIBarButtonItemStyleDone target:self action:@selector(onInvite:)];
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height) style:UITableViewStylePlain];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    [self.tableView registerClass:[WFCUConferenceMemberTableViewCell class] forCellReuseIdentifier:@"cell"];
+    [self.view addSubview:self.tableView];
+    
+    [self loadData];
+    [self.tableView reloadData];
+}
+
+- (void)loadData {
+    self.participants = [[NSMutableArray alloc] init];
+    
+    NSArray<WFAVParticipantProfile *> *ps =  [WFAVEngineKit sharedEngineKit].currentSession.participants;
+    for (WFAVParticipantProfile *p in ps) {
+        WFCUConferenceMember *member = [[WFCUConferenceMember alloc] init];
+        member.userId = p.userId;
+        member.isHost = [p.userId isEqualToString:[WFAVEngineKit sharedEngineKit].currentSession.host];
+        member.isVideoEnabled = !p.videoMuted;
+        member.isMe = NO;
+        
+        if(member.isHost) {
+            [self.participants insertObject:member atIndex:0];
+        } else {
+            [self.participants addObject:member];
+        }
+    }
+    
+    
+    WFCUConferenceMember *member = [[WFCUConferenceMember alloc] init];
+    member.userId = [WFCCNetworkService sharedInstance].userId;
+    member.isHost = [member.userId isEqualToString:[WFAVEngineKit sharedEngineKit].currentSession.host];
+    member.isVideoEnabled = ![WFAVEngineKit sharedEngineKit].currentSession.isVideoMuted;
+    member.isMe = YES;
+    
+    if(self.participants.count && self.participants[0].isHost) {
+        [self.participants insertObject:member atIndex:1];
+    } else {
+        [self.participants insertObject:member atIndex:0];
+    }
 }
 
 - (void)onClose:(id)sender {
@@ -44,4 +88,18 @@
     [self presentViewController:navi animated:YES completion:nil];
 }
 
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    WFCUConferenceMemberTableViewCell *cell = (WFCUConferenceMemberTableViewCell *)[tableView dequeueReusableCellWithIdentifier:@"cell"];
+    cell.member = self.participants[indexPath.row];
+    return cell;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 56;
+}
+
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.participants.count;
+}
 @end
