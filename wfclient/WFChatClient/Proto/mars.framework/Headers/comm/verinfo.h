54663f768073039ce5115327a945c74708428f6f
imhao183 <wildfirechat86@gmail.com>
2021-03-18 21:04:04 +0800

    解决清除会话消息后再加载后会话没有最后一条消息的问题
    Former-commit-id: 3e3a094fbb929f1619c1b20222a8572470929fcf

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "1a7fa5a3"
+#define MARS_REVISION "fe2dbfeb"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-03-06 21:24:02"
+#define MARS_BUILD_TIME "2021-03-18 20:53:43"
 #define MARS_TAG ""
 
 #endif
