1725c9b543a365706ca2ef7ec2a2e299c9c34bed
imhao183 <wildfirechat86@gmail.com>
2021-03-19 09:00:06 +0800

    更新协议栈
    Former-commit-id: 32b6ccc3820c8d7b3a048af52a70aed2a2c3b85b

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "fe2dbfeb"
+#define MARS_REVISION "297f8982"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-03-18 20:53:43"
+#define MARS_BUILD_TIME "2021-03-19 08:32:50"
 #define MARS_TAG ""
 
 #endif
