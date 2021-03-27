bfb75f5a7ceb06230ae56b60b82f1192070b9962
heavyrain2012 <heavyrain.lee@gmail.com>
2021-03-27 10:17:42 +0800

    更新协议栈，优化网络变化时连接速度

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "297f8982"
+#define MARS_REVISION "f2ed47f3"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-03-19 08:32:50"
+#define MARS_BUILD_TIME "2021-03-27 08:47:20"
 #define MARS_TAG ""
 
 #endif
