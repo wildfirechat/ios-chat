3302fccd5f54c37ea48c702d4523b979ee99da62
imhao183 <wildfirechat86@gmail.com>
2021-03-31 10:42:45 +0800

    域名切换到net

--- wfchat/WildFireChat/WFCConfig.m
+++ wfchat/WildFireChat/WFCConfig.m
@@ -10,7 +10,7 @@
 
 //可以是IP，可以是域名，如果是域名的话只支持主域名或www域名或im或imtest的二级域名，其它二级域名不支持！
 //例如：example.com或www.example.com或im.example.com或imtest.example.com是支持的；xx.example.com或xx.yy.example.com是不支持的。如果是专业版必须用域名，社区版建议也用域名。
-NSString *IM_SERVER_HOST = @"wildfirechat.cn";
+NSString *IM_SERVER_HOST = @"wildfirechat.net";
 
 
 // App Server默认使用的是8888端口，替换为自己部署的服务时需要注意端口别填错了，使用http访问appserver时，需要确保appserver的配置文件中的wfc.all_client_support_ssl参数为false
