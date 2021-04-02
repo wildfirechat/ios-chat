d10ccdbd32d813200b37eb40130f87c404be8856
imhao183 <wildfirechat86@gmail.com>
2021-04-02 13:12:46 +0800

    更新协议栈，解决修改群组extra字段时更新群组信息的问题
    Former-commit-id: 88a15d0c8d497542ef15d3910d4b960980d52d55

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "f2ed47f3"
+#define MARS_REVISION "f769f105"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-03-27 08:47:20"
+#define MARS_BUILD_TIME "2021-04-02 12:29:17"
 #define MARS_TAG ""
 
 #endif
