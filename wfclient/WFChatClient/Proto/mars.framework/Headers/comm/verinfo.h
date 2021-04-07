ed5085ec90d5576c3d6bb38595b1db9b1b336366
imndx <imndxx@gmail.com>
2021-04-07 08:27:49 +0800

    更新协议栈，修复同步草稿会导致会话最后一条消息消失的问题
    Former-commit-id: 8249dcaed51f0e73296c8ee04ae3950b26b557f7

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "1d5db5ec"
+#define MARS_REVISION "c5a3dd4f"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-04-04 21:10:42"
+#define MARS_BUILD_TIME "2021-04-06 20:36:09"
 #define MARS_TAG ""
 
 #endif
