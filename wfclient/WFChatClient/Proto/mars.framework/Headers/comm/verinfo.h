eae89365b3bd44ebefa60f0f52d153de68a301c4
imhao183 <wildfirechat86@gmail.com>
2021-04-19 13:04:44 +0800

    update proto
    Former-commit-id: 65a49a77f2688f34d363ed24b7f7420be6b2de57

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "e1c49b21"
+#define MARS_REVISION "90906dd4"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-04-16 17:39:58"
+#define MARS_BUILD_TIME "2021-04-19 12:34:34"
 #define MARS_TAG ""
 
 #endif
