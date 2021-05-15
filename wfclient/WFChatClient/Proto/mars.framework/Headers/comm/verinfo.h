deada0a7b0afebbe7d128619116417e9f4ec7847
imhao183 <wildfirechat86@gmail.com>
2021-05-15 09:37:55 +0800

    Revert "update proto"
    This reverts commit 0c0fddf3ef079220579039d7a467e1cbf4b8d32c.

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "b74507dd"
+#define MARS_REVISION "b09896bf"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-05-14 20:49:12"
+#define MARS_BUILD_TIME "2021-04-30 18:34:04"
 #define MARS_TAG ""
 
 #endif
