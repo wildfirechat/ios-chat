5f8aae20cf2bc21f819d1a39561eaae4a94616a5
imhao183 <wildfirechat86@gmail.com>
2021-06-05 11:02:06 +0800

    更新协议栈，解决发送文件名字超长问题

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -5,7 +5,7 @@
 #define MARS_REVISION "0f10ec4c"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-05-24 12:15:28"
+#define MARS_BUILD_TIME "2021-06-05 10:22:33"
 #define MARS_TAG ""
 
 #endif
