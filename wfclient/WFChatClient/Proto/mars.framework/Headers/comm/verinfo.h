e30964806f5778b641a5202346ce85f7ffcb5a4a
heavyrain2012 <heavyrain.lee@gmail.com>
2021-07-09 12:04:06 +0800

    更新协议栈，可以支持任意域名
    Former-commit-id: 03fa5a129956a01fbaad6bde40fbf45ceed9e630

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "99a59981"
+#define MARS_REVISION "d5ad245a"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-07-05 12:51:56"
+#define MARS_BUILD_TIME "2021-07-09 11:21:27"
 #define MARS_TAG ""
 
 #endif
