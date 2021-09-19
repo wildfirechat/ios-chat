8e261615f54080b8987075ed139a400c53428598
heavyrain2012 <heavyrain.lee@gmail.com>
2021-09-19 15:35:57 +0800

    多端登录被踢下线
    Former-commit-id: 617e254b295e0c66b7f6d747e124f74d0da507c4

--- wfchat/WildFireChat/Login/WFCLoginViewController.h
+++ wfchat/WildFireChat/Login/WFCLoginViewController.h
@@ -9,4 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface WFCLoginViewController : UIViewController
+//如果是因为多端登录被踢，提示原因。注意错误码kConnectionStatusKickedoff是IM服务2021.9.15之后的版本才支持，并且打开服务器端开关server.client_support_kickoff_event
+@property(nonatomic, assign)BOOL isKickedOff;
 @end
