925d010107b37cd5a45edd1ce5e93cc093d4c9ee
heavyrain2012 <heavyrain.lee@gmail.com>
2021-09-29 16:00:10 +0800

    修正上一个版本的协议栈错误

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "3c91e164"
+#define MARS_REVISION "817ff58d"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-09-27 11:40:38"
+#define MARS_BUILD_TIME "2021-09-29 15:51:52"
 #define MARS_TAG ""
 
 #endif
