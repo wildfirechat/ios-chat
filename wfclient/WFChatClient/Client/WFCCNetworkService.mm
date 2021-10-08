33c3a4b12162985a1c6574a5157ac95f095592b9
heavyrain2012 <heavyrain.lee@gmail.com>
2021-10-08 12:14:20 +0800

    debug模式下开始console日志

--- wfclient/WFChatClient/Client/WFCCNetworkService.mm
+++ wfclient/WFChatClient/Client/WFCCNetworkService.mm
@@ -446,7 +446,7 @@ static WFCCNetworkService * sharedSingleton = nil;
     // init xlog
 #if DEBUG
     xlogger_SetLevel(kLevelVerbose);
-    appender_set_console_log(false);
+    appender_set_console_log(true);
 #else
     xlogger_SetLevel(kLevelInfo);
     appender_set_console_log(false);
