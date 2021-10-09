83b34bf5381c42223ebe1825f536ec670223fb0c
heavyrain2012 <heavyrain.lee@gmail.com>
2021-10-09 21:21:04 +0800

    更新协议栈解决不能互踢的问题
    Former-commit-id: 82ddde886519938e86150f966a83e2ffdecc7325

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "825c1c94"
+#define MARS_REVISION "db1b4025"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-10-08 12:03:30"
+#define MARS_BUILD_TIME "2021-10-09 21:05:04"
 #define MARS_TAG ""
 
 #endif
