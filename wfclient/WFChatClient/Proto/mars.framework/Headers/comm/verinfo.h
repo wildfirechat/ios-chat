6a7d3ea00053499a2b6bc1026316edbb99e522c7
heavyrain2012 <heavyrain.lee@gmail.com>
2021-10-13 22:19:39 +0800

    1,更新协议栈；2添加lock接口
    Former-commit-id: 92f83e21cea98b8b562109338aece0e65d86a25e

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "db1b4025"
+#define MARS_REVISION "27a35fed"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-10-09 21:05:04"
+#define MARS_BUILD_TIME "2021-10-13 22:04:26"
 #define MARS_TAG ""
 
 #endif
