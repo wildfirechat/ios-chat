d8f07a7bd71440d5778a95084c512c26f63ebccd
heavyrain2012 <heavyrain.lee@gmail.com>
2021-10-20 15:15:17 +0800

    更新协议栈解决有时协议栈崩溃问题

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "27a35fed"
+#define MARS_REVISION "5947d52d"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-10-13 22:04:26"
+#define MARS_BUILD_TIME "2021-10-20 14:53:49"
 #define MARS_TAG ""
 
 #endif
