068a4f59a2e3fe53a8c2adadd8b90de41ef81830
heavyrain2012 <heavyrain.lee@gmail.com>
2021-10-28 08:53:07 +0800

    更新协议栈解决建立会话时缺失置顶信息问题

--- wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.framework/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "5834b219"
+#define MARS_REVISION "52b1381d"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-10-26 22:12:55"
+#define MARS_BUILD_TIME "2021-10-28 08:40:02"
 #define MARS_TAG ""
 
 #endif
