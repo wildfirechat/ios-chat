4e30c0bbf300afe06059bdb9da466f094e3fa5d3
heavyrain2012 <heavyrain.lee@gmail.com>
2021-12-21 11:04:56 +0800

    添加根据消息uid来获取消息的接口

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_arm64e/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_arm64e/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "3756b2bb"
+#define MARS_REVISION "20e959e3"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2021-12-20 09:08:34"
+#define MARS_BUILD_TIME "2021-12-21 10:34:43"
 #define MARS_TAG ""
 
 #endif
