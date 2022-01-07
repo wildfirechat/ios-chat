861dc95ab423c02c11cd4de6b2f973edd87002e5
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-01-07 13:52:13 +0800

    搜索消息可以按照消息类型搜索

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "99bad77f"
+#define MARS_REVISION "c00a7dae"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-01-04 10:30:37"
+#define MARS_BUILD_TIME "2022-01-07 13:48:39"
 #define MARS_TAG ""
 
 #endif
