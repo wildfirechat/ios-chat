7fe7dac4783fc509b50240015a23c52082e5e474
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-03-25 20:39:30 +0800

    解决特殊字符用户id发送媒体消息失败的问题

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "d43b2d4a"
+#define MARS_REVISION "6786b478"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-03-25 14:05:19"
+#define MARS_BUILD_TIME "2022-03-25 20:36:12"
 #define MARS_TAG ""
 
 #endif
