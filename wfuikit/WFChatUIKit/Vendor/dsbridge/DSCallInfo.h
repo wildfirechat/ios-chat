ce67acd89f80ddf9dabfd0ad45fc7509cdf47f91
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-05-16 16:08:32 +0800

    work platform

---
+++ wfuikit/WFChatUIKit/Vendor/dsbridge/DSCallInfo.h
@@ -0,0 +1,8 @@
+
+#import <Foundation/Foundation.h>
+
+@interface DSCallInfo : NSObject
+@property (nullable, nonatomic) NSString* method;
+@property (nullable, nonatomic) NSNumber* id;
+@property (nullable,nonatomic) NSArray * args;
+@end
