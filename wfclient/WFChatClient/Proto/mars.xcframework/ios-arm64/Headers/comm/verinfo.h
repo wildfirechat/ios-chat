97940af2c2eb4b18939b1816bd2f950767e9bb50
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-05-26 21:33:41 +0800

    remark use scope 26

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "a0476f31"
+#define MARS_REVISION "f0a9cef7"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-05-26 20:58:01"
+#define MARS_BUILD_TIME "2022-05-26 21:30:41"
 #define MARS_TAG ""
 
 #endif
