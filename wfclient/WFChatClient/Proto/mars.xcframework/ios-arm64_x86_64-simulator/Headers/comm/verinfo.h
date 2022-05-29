6f076519b7be9c0fabc2bdb010bd0c90a9de70b1
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-05-29 10:19:57 +0800

    更新协议栈，优化后台切换到前台连接的速度

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "02b02f21"
+#define MARS_REVISION "7023a587"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-05-27 09:51:44"
+#define MARS_BUILD_TIME "2022-05-29 10:03:43"
 #define MARS_TAG ""
 
 #endif
