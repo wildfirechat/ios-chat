30766f133e7625dd0383ce0a0c086a645faab5e8
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-06-02 14:31:32 +0800

    Revert "update proto"
    This reverts commit 5e0aa379c253c38db2fb072303f6d161146355a8 [formerly f38752dce7298b772c791df3090040d09ad87f06].
    
    
    Former-commit-id: 22547608842b10c9d3af29223bd676cc3d43edff

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "68734284"
+#define MARS_REVISION "7023a587"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-06-02 12:07:09"
+#define MARS_BUILD_TIME "2022-05-29 10:03:43"
 #define MARS_TAG ""
 
 #endif
