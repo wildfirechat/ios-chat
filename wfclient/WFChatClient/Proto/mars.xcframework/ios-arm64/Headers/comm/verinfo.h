7280ca199f3b401e20dcd30bd7f277a28bb04ed7
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-06-08 09:18:30 +0800

    更新协议栈，解决卸载重装未读数的问题

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "d58d11c3"
+#define MARS_REVISION "feb22a16"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-06-03 07:45:24"
+#define MARS_BUILD_TIME "2022-06-08 09:10:06"
 #define MARS_TAG ""
 
 #endif
