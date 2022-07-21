4415674ea42c2c4a37352a862831a48d7d12b0fd
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-07-21 15:08:45 +0800

    解决获取指定数目群组成员错误问题
    Former-commit-id: a41c26e03d5798785bd520dc055048123c0f11a5

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "5c5ed854"
+#define MARS_REVISION "b841b6de"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-07-17 18:26:23"
+#define MARS_BUILD_TIME "2022-07-21 15:01:34"
 #define MARS_TAG ""
 
 #endif
