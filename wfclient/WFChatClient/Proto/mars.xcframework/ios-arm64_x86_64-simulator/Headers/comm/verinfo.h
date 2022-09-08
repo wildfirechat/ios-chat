c704d247e5c38aaa143871276f707f407bb437a1
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-09-08 09:04:21 +0800

    更新协议栈，解决当删除草稿后会话时间错误问题

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "93c5447c"
+#define MARS_REVISION "5d358252"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-09-02 09:14:56"
+#define MARS_BUILD_TIME "2022-09-08 08:24:21"
 #define MARS_TAG ""
 
 #endif
