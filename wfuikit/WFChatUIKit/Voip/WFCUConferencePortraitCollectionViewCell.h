47f9f0a0cfcd5c6d185d677f5e29950887e9fec2
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-09-11 21:56:19 +0800

    解决多人音视频图标错误问题

---
+++ wfuikit/WFChatUIKit/Voip/WFCUConferencePortraitCollectionViewCell.h
@@ -0,0 +1,24 @@
+//
+//  WFCUConferencePortraitCollectionViewCell.h
+//  WFChatUIKit
+//
+//  Created by dali on 2020/1/20.
+//  Copyright © 2020 WildFireChat. All rights reserved.
+//
+#if WFCU_SUPPORT_VOIP
+#import <UIKit/UIKit.h>
+#import <WFChatClient/WFCChatClient.h>
+#import <WFAVEngineKit/WFAVEngineKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WFCUConferencePortraitCollectionViewCell : UICollectionViewCell
+@property (nonatomic, strong)WFCCUserInfo *userInfo;
+@property (nonatomic, strong)WFAVParticipantProfile *profile;
+
+@property (nonatomic, assign)CGFloat itemSize;
+@property (nonatomic, assign)CGFloat labelSize;
+@end
+
+NS_ASSUME_NONNULL_END
+#endif
