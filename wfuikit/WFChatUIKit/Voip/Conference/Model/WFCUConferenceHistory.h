cdb3089a195467bdec0a1ea283e6667845c3040c
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-09-16 12:02:24 +0800

    添加历史记录

---
+++ wfuikit/WFChatUIKit/Voip/Conference/Model/WFCUConferenceHistory.h
@@ -0,0 +1,23 @@
+//
+//  WFCUConferenceHistory.h
+//  WFChatUIKit
+//
+//  Created by Rain on 2022/9/16.
+//  Copyright © 2022 Tom Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class WFZConferenceInfo;
+@interface WFCUConferenceHistory : NSObject
+@property(nonatomic, strong)WFZConferenceInfo *conferenceInfo;
+@property(nonatomic, assign)int64_t timestamp;
+@property(nonatomic, assign)int duration;
+
++ (instancetype)fromDictionary:(NSDictionary *)dictionary;
+- (NSDictionary *)toDictionary;
+@end
+
+NS_ASSUME_NONNULL_END
