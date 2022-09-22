2b30e18ad8cf72edd6afe7343f36765fd22b9911
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-09-22 15:34:45 +0800

    会议UI优化

---
+++ wfuikit/WFChatUIKit/Voip/Conference/WFCUConferenceCollectionViewLayout.h
@@ -0,0 +1,17 @@
+//
+//  WFCUConferenceCollectionViewLayout.h
+//  WFChatUIKit
+//
+//  Created by Rain on 2022/9/21.
+//  Copyright © 2022 Tom Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WFCUConferenceCollectionViewLayout : UICollectionViewLayout
+
+@end
+
+NS_ASSUME_NONNULL_END
