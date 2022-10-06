f7227814cb418f1ca0521493a9d03f2329ffbfa9
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-10-06 21:05:49 +0800

    解决某些界面群组头像没有拼接问题

--- wfuikit/WFChatUIKit/ConversationList/SearchCell/WFCUSearchGroupTableViewCell.m
+++ wfuikit/WFChatUIKit/ConversationList/SearchCell/WFCUSearchGroupTableViewCell.m
@@ -110,7 +110,18 @@
         [attrStr appendAttributedString:string];
         self.haveMember.attributedText = attrStr;
     }
-    [self.portrait sd_setImageWithURL:[NSURL URLWithString:[groupInfo.portrait stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:[WFCUImage imageNamed:@"group_default_portrait"]];
+    
+    if (groupInfo.portrait.length) {
+        [self.portrait sd_setImageWithURL:[NSURL URLWithString:[groupInfo.portrait stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] placeholderImage:[WFCUImage imageNamed:@"group_default_portrait"]];
+    } else {
+        NSString *path = [WFCCUtilities getGroupGridPortrait:groupInfo.target width:80 generateIfNotExist:YES defaultUserPortrait:^UIImage *(NSString *userId) {
+            return [WFCUImage imageNamed:@"PersonalChat"];
+        }];
+        
+        if (path) {
+            [self.portrait sd_setImageWithURL:[NSURL fileURLWithPath:path] placeholderImage:[WFCUImage imageNamed:@"group_default_portrait"]];
+        }
+    }
 }
 
 @end
