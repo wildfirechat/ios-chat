5c380e311df01a14f891f79c8d1cb54c7c54fd9c
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-11-21 15:07:29 +0800

    解决查看机器人信息时崩溃的问题

--- wfuikit/WFChatUIKit/CommonVC/WFCUProfileTableViewController.m
+++ wfuikit/WFChatUIKit/CommonVC/WFCUProfileTableViewController.m
@@ -342,10 +342,6 @@
                 self.momentCell.selectionStyle = UITableViewCellSelectionStyleNone;
                 self.momentCell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
             }
-            
-            self.moreCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"more_cell"];
-            self.moreCell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-            self.moreCell.textLabel.text = WFCString(@"More");
         }
         
         if ([[WFCCIMService sharedWFCIMService] isMyFriend:self.userId]) {
@@ -416,6 +412,15 @@
     } else {
         [self.sendMessageCell addSubview:btn];
     }
+}
+
+- (UITableViewCell *)moreCell {
+    if(!_moreCell) {
+        _moreCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"more_cell"];
+        _moreCell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        _moreCell.textLabel.text = WFCString(@"More");
+    }
+    return _moreCell;
 }
 
 - (UIEdgeInsets)hiddenSeparatorLine:(UITableViewCell *)cell {
