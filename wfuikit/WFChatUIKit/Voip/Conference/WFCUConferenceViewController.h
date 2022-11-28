ab5edfdb5cf666bda4624fa41cc626aa243ffadc
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-11-28 13:57:43 +0800

    update
    Former-commit-id: ee07002d11ea9260b3e1257572d68de06a196a25

--- wfuikit/WFChatUIKit/Voip/Conference/WFCUConferenceViewController.h
+++ wfuikit/WFChatUIKit/Voip/Conference/WFCUConferenceViewController.h
@@ -16,5 +16,17 @@
 - (instancetype)initWithSession:(WFAVCallSession *)session conferenceInfo:(WFZConferenceInfo *)conferenceInfo;
 
 - (instancetype)initWithConferenceInfo:(WFZConferenceInfo *)conferenceInfo muteAudio:(BOOL)muteAudio muteVideo:(BOOL)muteVideo;
+
+- (instancetype)initWithCallId:(NSString *_Nullable)callId
+                     audioOnly:(BOOL)audioOnly
+                           pin:(NSString *_Nullable)pin
+                          host:(NSString *_Nullable)host
+                         title:(NSString *_Nullable)title
+                          desc:(NSString *_Nullable)desc
+                      audience:(BOOL)audience
+                      advanced:(BOOL)advanced
+                        record:(BOOL)record
+                        moCall:(BOOL)moCall
+                         extra:(NSString *)extra;
 @end
 #endif
