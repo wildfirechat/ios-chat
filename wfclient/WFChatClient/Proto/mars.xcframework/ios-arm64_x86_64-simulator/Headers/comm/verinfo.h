4de4254c94874f1c5d08c09ad143559c37c2ef49
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-12-26 15:47:09 +0800

    update proto

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "3262826c"
+#define MARS_REVISION "41792f20"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-12-23 19:36:00"
+#define MARS_BUILD_TIME "2022-12-26 15:37:26"
 #define MARS_TAG ""
 
 #endif
