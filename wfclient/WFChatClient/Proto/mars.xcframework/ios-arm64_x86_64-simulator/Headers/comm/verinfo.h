d235bc844575711f18ffb06d2b053835041a3c9e
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2023-01-29 18:25:35 +0800

    解决语音消息播放后还有红点的问题
    Former-commit-id: 8c5f693ae0967c5688721307551a85826db21895

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "a1a347ae"
+#define MARS_REVISION "efbb0bfb"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-12-28 13:01:09"
+#define MARS_BUILD_TIME "2023-01-29 18:21:34"
 #define MARS_TAG ""
 
 #endif
