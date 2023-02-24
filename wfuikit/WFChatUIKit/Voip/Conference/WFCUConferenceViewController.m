8aa3e1f3489b96dd7fa3f0860f81c51d9c3d7871
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2023-02-24 16:36:40 +0800

    优化会议UI

--- wfuikit/WFChatUIKit/Voip/Conference/WFCUConferenceViewController.m
+++ wfuikit/WFChatUIKit/Voip/Conference/WFCUConferenceViewController.m
@@ -2001,7 +2001,7 @@
 
 - (void)reloadParticipantCollectionView {
     [self.participantCollectionView reloadData];
-    dispatch_async(dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self updateVideoStreams];
     });
 }
