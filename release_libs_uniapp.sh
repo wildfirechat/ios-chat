bc38cd836c41fb8532e1617dd6c61aa369a62752
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2023-04-04 11:51:33 +0800

    add uniapp sdk release script
    Former-commit-id: f50868cdeab6f06f99282fbc268107d55c485878

---
+++ release_libs_uniapp.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+TARGET_CLIENT_NAME=WFChatClient
+TARGET_UIKIT_NAME=WFChatUIKit
+
+CURRENT_PATH=`pwd`
+
+PRG="$0"
+PRGDIR=`dirname "$PRG"`
+cd $PRGDIR
+
+BUILD_DIR=`pwd`"/build"
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
+
+#创建输出目录，并删除之前的framework文件
+UNIVERSAL_OUTPUT_FOLDER=`pwd`"/Libs&Resources"
+rm -rf ${UNIVERSAL_OUTPUT_FOLDER}
+mkdir -p ${UNIVERSAL_OUTPUT_FOLDER}
+
+#清理
+cd wfclient
+xcodebuild -target ${TARGET_CLIENT_NAME} ONLY_ACTIVE_ARCH=YES -arch arm64 -configuration Release -sdk iphoneos BUILD_DIR="${BUILD_DIR}" clean
+xcodebuild -target ${TARGET_CLIENT_NAME} ONLY_ACTIVE_ARCH=NO -configuration Release -sdk iphonesimulator BUILD_DIR="${BUILD_DIR}" clean
+cd ../wfuikit
+xcodebuild -target ${TARGET_UIKIT_NAME} ONLY_ACTIVE_ARCH=YES -arch arm64 -configuration Release -sdk iphoneos BUILD_DIR="${BUILD_DIR}" clean
+xcodebuild -target ${TARGET_UIKIT_NAME} ONLY_ACTIVE_ARCH=NO -configuration Release -sdk iphonesimulator BUILD_DIR="${BUILD_DIR}" clean
+
+
+#编译真机的Framework
+cd ../wfclient
+xcodebuild -target ${TARGET_CLIENT_NAME} ONLY_ACTIVE_ARCH=YES -arch arm64 -configuration Release -sdk iphoneos BUILD_DIR="${BUILD_DIR}" build
+cd ../wfuikit
+xcodebuild -target ${TARGET_UIKIT_NAME} ONLY_ACTIVE_ARCH=YES -arch arm64 -configuration Release -sdk iphoneos BUILD_DIR="${BUILD_DIR}" build
+
+cd ..
+xcodebuild -create-xcframework -framework "${BUILD_DIR}"/Release-iphoneos/"${TARGET_CLIENT_NAME}".framework  -output "${UNIVERSAL_OUTPUT_FOLDER}"/${TARGET_CLIENT_NAME}.xcframework
+xcodebuild -create-xcframework -framework "${BUILD_DIR}"/Release-iphoneos/"${TARGET_UIKIT_NAME}".framework  -output "${UNIVERSAL_OUTPUT_FOLDER}"/${TARGET_UIKIT_NAME}.xcframework
+
+rm -rf $BUILD_DIR
+
+##依赖
+cp -af wfuikit/WFChatUIKit/AVEngine/WebRTC.xcframework ${UNIVERSAL_OUTPUT_FOLDER}
+rm -rf ${UNIVERSAL_OUTPUT_FOLDER}/WebRTC.xcframework/ios-*-simulator
+cp -af wfuikit/WFChatUIKit/AVEngine/WFAVEngineKit.xcframework ${UNIVERSAL_OUTPUT_FOLDER}
+rm -rf ${UNIVERSAL_OUTPUT_FOLDER}/WFAVEngineKit.xcframework/ios-*-simulator
+cp -af wfuikit/WFChatUIKit/Vendor/ZLPhotoBrowser/ZLPhotoBrowser.xcframework ${UNIVERSAL_OUTPUT_FOLDER}
+rm -rf ${UNIVERSAL_OUTPUT_FOLDER}/ZLPhotoBrowser.xcframework/ios-*-simulator
+cp -af wfuikit/WFChatUIKit/Vendor/SDWebImage/SDWebImage.xcframework ${UNIVERSAL_OUTPUT_FOLDER}
+rm -rf ${UNIVERSAL_OUTPUT_FOLDER}/SDWebImage.xcframework/ios-*-simulator
+
+#打开合并后的文件夹
+open "${UNIVERSAL_OUTPUT_FOLDER}"
+
+cd $CURRENT_PATH
