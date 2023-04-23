eec093504b9555a2c2feaf944ec32d57124f83ae
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2023-04-23 16:17:40 +0800

    更新协议栈解决取消发送中消息失败问题及某些情况下多端未读数同步不正确问题
    Former-commit-id: 4fb1412f2f93867b95909c6832bd53a8110c996e

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "fb3a616b"
+#define MARS_REVISION "c31abdb1"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2023-04-18 09:58:29"
+#define MARS_BUILD_TIME "2023-04-23 16:09:05"
 #define MARS_TAG ""
 
 #endif
