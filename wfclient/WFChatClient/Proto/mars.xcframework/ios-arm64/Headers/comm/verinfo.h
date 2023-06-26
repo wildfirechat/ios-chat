81bdeedfe20db5be3aa4487297f344246424e194
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2023-06-26 11:23:32 +0800

    解决好友请求不显示的问题

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "e70eae9c"
+#define MARS_REVISION "1fc0b6ae"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2023-06-21 16:02:46"
+#define MARS_BUILD_TIME "2023-06-26 11:18:49"
 #define MARS_TAG ""
 
 #endif
