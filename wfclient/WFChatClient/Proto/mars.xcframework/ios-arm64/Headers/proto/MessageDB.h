413b18d684f2e56594f8bfee96c824e05fc78e76
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2023-07-11 10:09:29 +0800

    更新协议栈解决connect时返回时间不精确问题
    Former-commit-id: 3860ba44e9d6ca44e8498d97a108898ec80c8dba

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/proto/MessageDB.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/proto/MessageDB.h
@@ -87,7 +87,7 @@ namespace mars {
           
             TMessage GetMessageById(long messageId);
             TMessage GetMessageByUid(long long messageUid);
-            int64_t GetOldestMessageTimestamp();
+            int64_t GetNewestMessageTimestamp();
           
             
           
