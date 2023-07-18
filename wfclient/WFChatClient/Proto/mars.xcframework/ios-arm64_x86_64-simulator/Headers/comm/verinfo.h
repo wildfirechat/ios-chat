272676ce59d48c7d1e77734918126f275a4c211e
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2023-07-18 19:46:54 +0800

    更新协议栈，解决插入消息toUsers多个用户无法正确处理的问题
    Former-commit-id: 01d61744beac56c7456e50d1cec5e817e3c57f8e

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "efb4d8f1"
+#define MARS_REVISION "1ad78d0d"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2023-07-11 10:05:27"
+#define MARS_BUILD_TIME "2023-07-18 19:05:00"
 #define MARS_TAG ""
 
 #endif
