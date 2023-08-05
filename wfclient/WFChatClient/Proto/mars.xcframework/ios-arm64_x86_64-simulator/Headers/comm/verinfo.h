582107dc5903bff401743a36dbefb31b43487e18
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2023-08-05 13:58:22 +0800

    更新协议栈，解决插入消息后@信息丢失问题

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "ba50e2e5"
+#define MARS_REVISION "33ce6a1e"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2023-07-30 15:16:38"
+#define MARS_BUILD_TIME "2023-08-05 13:20:41"
 #define MARS_TAG ""
 
 #endif
