060ec6af4d103bcebfe933fdd27659f7811796a8
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2023-08-21 19:50:59 +0800

    更新协议栈，解决密聊崩溃问题

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "8c2ddee6"
+#define MARS_REVISION "d43d75a0"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2023-08-19 22:18:46"
+#define MARS_BUILD_TIME "2023-08-21 19:44:53"
 #define MARS_TAG ""
 
 #endif
