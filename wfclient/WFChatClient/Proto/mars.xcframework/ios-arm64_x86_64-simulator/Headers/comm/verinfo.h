c9c2f9b1a128d933b24113391af6725eb7edb23c
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2023-09-01 10:33:25 +0800

    更新iOS协议栈

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "d43d75a0"
+#define MARS_REVISION "b369df4f"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2023-08-21 19:44:53"
+#define MARS_BUILD_TIME "2023-09-01 10:21:25"
 #define MARS_TAG ""
 
 #endif
