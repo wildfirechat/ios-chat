935cf0a3af55d8d05e4b94c9a0a19bd94ede7686
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2023-10-13 10:48:36 +0800

    群公告UI优化
    Former-commit-id: e86caecc701932008af949ed5266b0d8c68a60ae

--- wfuikit/WFChatUIKit/ConversationList/ViewController/WFCUGroupAnnouncementViewController.m
+++ wfuikit/WFChatUIKit/ConversationList/ViewController/WFCUGroupAnnouncementViewController.m
@@ -13,14 +13,19 @@
 #import "MBProgressHUD.h"
 #import "WFCUImage.h"
 #import "WFCUUtilities.h"
+#import "UIView+Toast.h"
 
 @interface WFCUGroupAnnouncementViewController () <UITextViewDelegate>
 @property(nonatomic, strong)UIImageView *portraitView;
 @property(nonatomic, strong)UILabel *nameLabel;
 @property(nonatomic, strong)UILabel *timeLabel;
 
+@property(nonatomic, strong)UILabel *maxLengthLabel;
+
 @property(nonatomic, strong)UITextView *textView;
 @end
+
+#define MAX_TEXT_LENGTH 2000
 
 @implementation WFCUGroupAnnouncementViewController
 
@@ -28,27 +33,36 @@
     [super viewDidLoad];
     self.view.backgroundColor = [WFCUConfigManager globalManager].backgroudColor;
     int offset = 0;
+    CGFloat headHeight;
     if (self.announcement.author.length && self.announcement.text.length) {
-        self.portraitView = [[UIImageView alloc] initWithFrame:CGRectMake(16, [WFCUUtilities wf_navigationFullHeight] + 16, 48, 48)];
+        UIView *headView = [[UIView alloc] initWithFrame:CGRectMake(0, [WFCUUtilities wf_navigationFullHeight], self.view.bounds.size.width, 80)];
+        
+        self.portraitView = [[UIImageView alloc] initWithFrame:CGRectMake(16, 16, 48, 48)];
         WFCCUserInfo *author = [[WFCCIMService sharedWFCIMService] getUserInfo:self.announcement.author refresh:NO];
         [self.portraitView sd_setImageWithURL:[NSURL URLWithString:author.portrait] placeholderImage: [WFCUImage imageNamed:@"PersonalChat"]];
         
-        self.nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(80, [WFCUUtilities wf_navigationFullHeight] + 16, self.view.bounds.size.width - 80 - 16, 20)];
+        self.nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(80, 16, self.view.bounds.size.width - 80 - 16, 20)];
         self.nameLabel.text = author.displayName;
         
-        self.timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(80, [WFCUUtilities wf_navigationFullHeight] + 48, self.view.bounds.size.width - 80 - 16, 14)];
+        self.timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(80, 48, self.view.bounds.size.width - 80 - 16, 14)];
         NSDate *date = [NSDate dateWithTimeIntervalSince1970:self.announcement.timestamp/1000];
         self.timeLabel.text = date.description;
         self.timeLabel.font = [UIFont systemFontOfSize:14];
         self.timeLabel.textColor = [UIColor grayColor];
         
         
-        [self.view addSubview:self.portraitView];
-        [self.view addSubview:self.nameLabel];
-        [self.view addSubview:self.timeLabel];
+        [headView addSubview:self.portraitView];
+        [headView addSubview:self.nameLabel];
+        [headView addSubview:self.timeLabel];
+        [self.view addSubview:headView];
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onTapHeaderView:)];
+        [headView addGestureRecognizer:tap];
+        
         offset = 80;
+        headHeight = offset;
     } else {
         offset = 16;
+        headHeight = offset;
     }
     
     
@@ -73,29 +87,129 @@
         self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:WFCString(@"Edit") style:UIBarButtonItemStyleDone target:self action:@selector(onEditBtn:)];
     }
     
-    self.textView = [[UITextView alloc] initWithFrame:CGRectMake(16, [WFCUUtilities wf_navigationFullHeight] + offset + 1, self.view.bounds.size.width - 31, self.view.bounds.size.height - 81 - [WFCUUtilities wf_navigationFullHeight] - [WFCUUtilities wf_safeDistanceBottom] - hintSize)];
+    CGFloat maxLenLabelHeight = 24;
+    
+    CGFloat textHeigh = self.view.bounds.size.height - headHeight - [WFCUUtilities wf_navigationFullHeight] - [WFCUUtilities wf_safeDistanceBottom] - hintSize - maxLenLabelHeight;
+    self.textView = [[UITextView alloc] initWithFrame:CGRectMake(16, [WFCUUtilities wf_navigationFullHeight] + offset + 1, self.view.bounds.size.width - 32, textHeigh)];
     self.textView.editable = NO;
     self.textView.text = self.announcement.text;
     self.textView.font = [UIFont systemFontOfSize:[UIFont systemFontSize]];
     self.textView.delegate = self;
     [self.view addSubview:self.textView];
+    offset += textHeigh;
+    offset += 4;
+    
+    self.maxLengthLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.view.bounds.size.width - 80 - 16, [WFCUUtilities wf_navigationFullHeight] + offset, 80, maxLenLabelHeight - 4)];
+    self.maxLengthLabel.font = [UIFont systemFontOfSize:12];
+    self.maxLengthLabel.textColor = [UIColor grayColor];
+    self.maxLengthLabel.textAlignment = NSTextAlignmentRight;
+    [self.view addSubview:self.maxLengthLabel];
+    [self updateMaxLengthLabel];
+    self.maxLengthLabel.hidden = YES;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)updateMaxLengthLabel {
+    self.maxLengthLabel.text = [NSString stringWithFormat:@"%d/%d", self.textView.text.length, MAX_TEXT_LENGTH];
 }
-*/
+
+- (void)onTapHeaderView:(id)sender {
+    if([self.textView isFirstResponder]) {
+        [self.textView resignFirstResponder];
+    }
+}
+
 - (void)onEditBtn:(id)sender {
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:WFCString(@"Save") style:UIBarButtonItemStyleDone target:self action:@selector(onSaveBtn:)];
     self.navigationItem.rightBarButtonItem.enabled = NO;
     self.textView.editable = YES;
     [self.textView becomeFirstResponder];
     self.textView.selectedRange = NSMakeRange(self.announcement.text.length, 0);
+    self.maxLengthLabel.hidden = NO;
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillHide:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+}
+
+- (void)keyboardWillShow:(NSNotification *)notification {
+    if (![self.textView isFirstResponder]) {
+        return;
+    }
+    NSDictionary *userInfo = [notification userInfo];
+    NSValue *value = [userInfo objectForKey:UIKeyboardFrameEndUserInfoKey];
+    CGRect keyboardRect = [value CGRectValue];
+    CGFloat duration = [[userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] floatValue];
+    
+    int offset = 0;
+    CGFloat headHeight;
+    if (self.announcement.author.length && self.announcement.text.length) {
+        offset = 80;
+        headHeight = offset;
+    } else {
+        offset = 16;
+        headHeight = offset;
+    }
+    
+    
+    CGFloat hintSize = self.isManager?0:40;
+    
+    CGFloat maxLenLabelHeight = 24;
+    
+    CGFloat textHeigh = self.view.bounds.size.height - headHeight - [WFCUUtilities wf_navigationFullHeight] - [WFCUUtilities wf_safeDistanceBottom] - hintSize - maxLenLabelHeight - keyboardRect.size.height;
+    CGRect textViewFrame = self.textView.frame;
+    textViewFrame.size.height = textHeigh;
+    
+    
+    offset += textHeigh;
+    offset += 4;
+    
+    CGRect maxLenFrame = self.maxLengthLabel.frame;
+    maxLenFrame.origin.y = [WFCUUtilities wf_navigationFullHeight] + offset + 1;
+    
+    
+    [UIView animateWithDuration:duration animations:^{
+        self.textView.frame = textViewFrame;
+        self.maxLengthLabel.frame = maxLenFrame;
+    }];
+}
+
+- (void)keyboardWillHide:(NSNotification *)notification {
+    NSDictionary *userInfo = [notification userInfo];
+    CGFloat duration = [[userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] floatValue];
+    
+    int offset = 0;
+    CGFloat headHeight;
+    if (self.announcement.author.length && self.announcement.text.length) {
+        offset = 80;
+        headHeight = offset;
+    } else {
+        offset = 16;
+        headHeight = offset;
+    }
+    
+    CGFloat hintSize = self.isManager?0:40;
+    CGFloat maxLenLabelHeight = 24;
+    
+    CGFloat textHeigh = self.view.bounds.size.height - headHeight - [WFCUUtilities wf_navigationFullHeight] - [WFCUUtilities wf_safeDistanceBottom] - hintSize - maxLenLabelHeight;
+    CGRect textViewFrame = self.textView.frame;
+    textViewFrame.size.height = textHeigh;
+    
+    offset += textHeigh;
+    offset += 4;
+    
+    CGRect maxLenFrame = self.maxLengthLabel.frame;
+    maxLenFrame.origin.y = [WFCUUtilities wf_navigationFullHeight] + offset;
+    
+    
+    [UIView animateWithDuration:duration animations:^{
+        self.textView.frame = textViewFrame;
+        self.maxLengthLabel.frame = maxLenFrame;
+    }];
 }
 
 - (void)onSaveBtn:(id)sender {
@@ -129,10 +243,25 @@
 
 #pragma mark - UITextViewDelegate
 - (void)textViewDidChange:(UITextView *)textView {
+    [self updateMaxLengthLabel];
     if ([self.announcement.text isEqualToString:textView.text]) {
         self.navigationItem.rightBarButtonItem.enabled = NO;
     } else {
         self.navigationItem.rightBarButtonItem.enabled = YES;
     }
+}
+
+-(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    NSString *newText = [textView.text stringByReplacingCharactersInRange:range withString:text];
+    if(newText.length > MAX_TEXT_LENGTH) {
+        [self.view makeToast:@"超过大小限制"];
+        return NO;
+    }
+    
+    return YES;
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 @end
