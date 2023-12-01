899caa1b39ac1098afec57acef6dfc5585bf9852
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2023-12-01 19:27:50 +0800

    更新协议栈解决特殊字符的搜索问题
    Former-commit-id: 17b94c298db2ecb127a6656e81892ca4226e6f91

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "27a3c6ab"
+#define MARS_REVISION "a4c19f35"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2023-11-30 11:11:08"
+#define MARS_BUILD_TIME "2023-12-01 19:19:17"
 #define MARS_TAG ""
 
 #endif
