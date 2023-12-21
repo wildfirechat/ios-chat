bd9e4ae29284b01b9387c2241941efeec2fe4fb1
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2023-12-21 20:09:20 +0800

    解决上一个协议栈引入的消息自动删除问题
    Former-commit-id: 65a2a1fff9535c2e8933991527bd0adc254677ca

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "548dc3a7"
+#define MARS_REVISION "d738c930"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2023-12-20 21:02:27"
+#define MARS_BUILD_TIME "2023-12-21 20:05:19"
 #define MARS_TAG ""
 
 #endif
