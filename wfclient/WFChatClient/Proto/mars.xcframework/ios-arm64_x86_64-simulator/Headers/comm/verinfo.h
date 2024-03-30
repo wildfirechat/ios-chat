06d2127a51b403c7928dcf214ec9239edff29b55
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2024-03-30 20:55:08 +0800

    支持群组标记删除（需要服务器支持）

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "441f07c0"
+#define MARS_REVISION "b14d890a"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2024-03-14 16:22:53"
+#define MARS_BUILD_TIME "2024-03-30 20:40:45"
 #define MARS_TAG ""
 
 #endif
