c5bc038a36d45bfa313459e6929a4b9a9402ca3b
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2024-04-23 09:54:12 +0800

    更新协议栈，修改内容为：更新消息内容时，如果消息内容为不存储的，则删除旧消息

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "b14d890a"
+#define MARS_REVISION "e4ffaeac"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2024-03-30 20:40:45"
+#define MARS_BUILD_TIME "2024-04-23 09:49:06"
 #define MARS_TAG ""
 
 #endif
