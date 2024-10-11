0ac34259bc196d1bf4385920e14056492f008968
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2024-10-11 18:44:53 +0800

    解决超大群进入会话卡顿问题

--- wfuikit/WFChatUIKit/MessageList/ViewController/WFCUMessageListViewController.m
+++ wfuikit/WFChatUIKit/MessageList/ViewController/WFCUMessageListViewController.m
@@ -289,11 +289,15 @@
     self.chatInputBar.draft = info.draft;
     
     if (self.conversation.type == Group_Type) {
-        dispatch_async(dispatch_get_main_queue(), ^{
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{
             NSArray<WFCCGroupMember *> *groupMembers = [[WFCCIMService sharedWFCIMService] getGroupMembers:self.conversation.target forceUpdate:NO];
             NSMutableArray *memberIds = [[NSMutableArray alloc] init];
             for (WFCCGroupMember *member in groupMembers) {
                 [memberIds addObject:member.memberId];
+                if (memberIds.count >= 100) {
+                    //预加载100名群成员
+                    break;
+                }
             }
             [[WFCCIMService sharedWFCIMService] getUserInfos:memberIds inGroup:self.conversation.target];
         });
