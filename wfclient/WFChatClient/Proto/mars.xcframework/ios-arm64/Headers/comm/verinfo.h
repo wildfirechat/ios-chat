8e129e47437086e6a0c70cefd65faa3baace593c
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2024-12-01 13:33:02 +0800

    更新协议栈，协议栈内置sqlite添加命名空间，避免与其他sqlite库产生冲突

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "39700ea6"
+#define MARS_REVISION "9df99983"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2024-11-09 13:39:48"
+#define MARS_BUILD_TIME "2024-12-01 13:22:43"
 #define MARS_TAG ""
 
 #endif
