b5f1ce822fbec6881e11585eab7160abf51ae52b
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2025-07-18 12:21:16 +0800

    callkit推送，如果应用被杀，用推送来的用户名（需要更新推送服务）

--- wfchat/WildFireChat/WFCCallKitManager.m
+++ wfchat/WildFireChat/WFCCallKitManager.m
@@ -68,12 +68,19 @@
     NSDictionary *wfc = payload.dictionaryPayload[@"wfc"];
     if(wfc) {
         NSString *sender = wfc[@"sender"];
+        NSString *senderName = wfc[@"senderName"];
+        if(!senderName.length) {
+            senderName = sender;
+        }
         NSString *pushData = wfc[@"pushData"];
         NSDictionary *pd = [NSJSONSerialization JSONObjectWithData:[pushData dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:nil];
         BOOL audioOnly = [pd[@"audioOnly"] boolValue];
         NSString *callId = pd[@"callId"];
-        WFCCUserInfo *userInfo = [[WFCCIMService sharedWFCIMService] getUserInfo:sender refresh:NO];
-        [self reportIncomingCallWithTitle:userInfo.displayName Sid:sender audioOnly:audioOnly callId:callId];
+        NSString *name = [[WFAVEngineKit sharedEngineKit] getUserDisplayName:sender];
+        if(!name.length || [name rangeOfString:@"<"].location == 0) {
+            name = senderName;
+        }
+        [self reportIncomingCallWithTitle:name Sid:sender audioOnly:audioOnly callId:callId];
     }
 }
 
