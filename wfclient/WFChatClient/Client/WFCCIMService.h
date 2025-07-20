a6497097f2f22abc0464bb9e4774ed41969330a7
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2025-07-20 11:42:33 +0800

    更新注释

--- wfclient/WFChatClient/Client/WFCCIMService.h
+++ wfclient/WFChatClient/Client/WFCCIMService.h
@@ -1433,7 +1433,7 @@ typedef NS_ENUM(NSInteger, WFCCFileRecordOrder) {
  @param endTime 结束时间。
  
  @return 这个时间范围内每天的消息数。
- @discussion 如果获取某天的消息，可以用 getMessagesV2:contentTypes:from:count:withUser:success:error: 方法，其中from可以用当天时间的秒数（也可用消息ID）。如果获取这天的消息，以降序排列，from就是这天的最后一秒的时间，然后count为正值。如果以升序排列，from为这一天的第一秒，count值为负值。
+ @discussion 如果获取某天的消息，可以用 getMessagesV2:contentTypes:from:count:withUser:success:error: 方法，其中from可以用当天时间的毫秒数（也可用消息ID）。如果获取这天的消息，以降序排列，from就是这天的最后一秒毫秒数，然后count为正值。如果以升序排列，from为这一天的第一秒，count值为负值。
  */
 - (NSDictionary<NSString *, NSNumber *> *)getMessageCountByDay:(WFCCConversation *)conversation contentTypes:(NSArray<NSNumber *> *)contentTypes startTime:(int64_t)startTime endTime:(int64_t)endTime;
 #pragma mark - 用户相关
