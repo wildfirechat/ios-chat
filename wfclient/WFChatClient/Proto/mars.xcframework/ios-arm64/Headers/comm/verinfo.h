a253b35ef297b1940da4fabc94f242fabfb389d7
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2025-09-06 15:43:00 +0800

    升级协议栈支持单人会话机器人发消息
    Former-commit-id: 720ae5b3fdab433ecd55fbcc757bde41c2cb8c84

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "a419d9b6"
+#define MARS_REVISION "4d9096dd"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2025-08-27 11:18:17"
+#define MARS_BUILD_TIME "2025-09-06 13:00:58"
 #define MARS_TAG ""
 
 #endif
