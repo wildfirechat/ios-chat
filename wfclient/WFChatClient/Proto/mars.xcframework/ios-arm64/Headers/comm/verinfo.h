ba135a62ef0d1bcfef1633418bf51e0e490222d3
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2025-09-11 11:40:44 +0800

    更新协议栈，添加获取远程新消息接口

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "4d9096dd"
+#define MARS_REVISION "7c601a55"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2025-09-06 13:00:58"
+#define MARS_BUILD_TIME "2025-09-11 11:35:46"
 #define MARS_TAG ""
 
 #endif
