398322a751ec658cceb1beb102736493887a1906
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2025-10-28 20:13:02 +0800

    更新协议栈解决删除会话后再次出现的问题
    Former-commit-id: 7e85572d9ad0a68f5a3435db5e57da3a0d41bf1c

--- wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
+++ wfclient/WFChatClient/Proto/mars.xcframework/ios-arm64_x86_64-simulator/Headers/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "8f616851"
+#define MARS_REVISION "5b5d1d84"
 #define MARS_PATH "firechat"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2025-10-25 15:09:46"
+#define MARS_BUILD_TIME "2025-10-28 20:09:54"
 #define MARS_TAG ""
 
 #endif
