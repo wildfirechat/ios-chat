40af42c5fae39ae921827136c999b5f0128c1131
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2026-01-05 20:20:31 +0800

    用户资料页面点邮箱地址发送邮件

--- wfuikit/WFChatUIKit/CommonVC/WFCUProfileMoreTableViewController.m
+++ wfuikit/WFChatUIKit/CommonVC/WFCUProfileMoreTableViewController.m
@@ -24,15 +24,17 @@
 #import "WFCUUserMessageListViewController.h"
 #import "WFCUImage.h"
 #import "WFCUGroupTableViewController.h"
+#import <MessageUI/MessageUI.h>
 
 
-@interface WFCUProfileMoreTableViewController () <UITableViewDelegate, UITableViewDataSource>
+@interface WFCUProfileMoreTableViewController () <UITableViewDelegate, UITableViewDataSource, MFMailComposeViewControllerDelegate>
 
 
 @property (strong, nonatomic)UITableViewCell *commonGroupCell;
 @property (nonatomic, strong)UITableView *tableView;
 @property (nonatomic, strong)NSMutableArray<UITableViewCell *> *cells;
 @property (nonatomic, strong)WFCCUserInfo *userInfo;
+@property (nonatomic, strong)UITableViewCell *emailCell; // 邮箱 cell
 @end
 
 @implementation WFCUProfileMoreTableViewController
@@ -67,42 +69,44 @@
 
 - (void)loadData {
     self.cells = [[NSMutableArray alloc] init];
-    
+
     if (self.userInfo.mobile.length > 0) {
         UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
         cell.textLabel.text = @"电话";
         cell.detailTextLabel.text = self.userInfo.mobile;
         [self.cells addObject:cell];
     }
-    
+
     if (self.userInfo.email.length > 0) {
         UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
         cell.textLabel.text = @"邮箱";
         cell.detailTextLabel.text = self.userInfo.email;
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator; // 添加箭头表示可点击
+        self.emailCell = cell; // 保存邮箱 cell 的引用
         [self.cells addObject:cell];
     }
-    
+
     if (self.userInfo.address.length) {
         UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
         cell.textLabel.text = @"地址";
         cell.detailTextLabel.text = self.userInfo.address;
         [self.cells addObject:cell];
     }
-    
+
     if (self.userInfo.company.length) {
         UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
         cell.textLabel.text = @"公司";
         cell.detailTextLabel.text = self.userInfo.company;
         [self.cells addObject:cell];
     }
-    
+
     if (self.userInfo.social.length) {
         UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
         cell.textLabel.text = @"社交账号";
         cell.detailTextLabel.text = self.userInfo.social;
         [self.cells addObject:cell];
     }
-    
+
     [self.tableView reloadData];
 }
 
@@ -174,12 +178,78 @@
 }
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    if([tableView cellForRowAtIndexPath:indexPath] == self.commonGroupCell) {
+    UITableViewCell *selectedCell = [tableView cellForRowAtIndexPath:indexPath];
+
+    if(selectedCell == self.commonGroupCell) {
         WFCUGroupTableViewController *groupsVC = [[WFCUGroupTableViewController alloc] init];
         groupsVC.groupIds = self.commonGroupIds;
         groupsVC.titleString = @"我和他的共同群组";
         [self.navigationController pushViewController:groupsVC animated:YES];
+    } else if (selectedCell == self.emailCell) {
+        // 点击邮箱 cell，发送邮件
+        [self sendEmailTo:self.userInfo.email];
     }
+
+    // 取消选中状态
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+// 发送邮件
+- (void)sendEmailTo:(NSString *)email {
+    if (!email || email.length == 0) {
+        return;
+    }
+
+    // 检查设备是否支持发送邮件
+    if ([MFMailComposeViewController canSendMail]) {
+        // 使用 MFMailComposeViewController
+        MFMailComposeViewController *mailComposeVC = [[MFMailComposeViewController alloc] init];
+        mailComposeVC.mailComposeDelegate = self;
+        [mailComposeVC setToRecipients:@[email]];
+
+        [self presentViewController:mailComposeVC animated:YES completion:nil];
+    } else {
+        // 如果不支持，使用 mailto: URL scheme
+        NSString *emailString = [NSString stringWithFormat:@"mailto:%@", email];
+        NSURL *emailURL = [NSURL URLWithString:emailString];
+
+        if ([[UIApplication sharedApplication] canOpenURL:emailURL]) {
+            [[UIApplication sharedApplication] openURL:emailURL options:@{} completionHandler:nil];
+        } else {
+            // 如果都无法发送，显示提示
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示"
+                                                                           message:@"您的设备不支持发送邮件"
+                                                                    preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil];
+            [alert addAction:okAction];
+            [self presentViewController:alert animated:YES completion:nil];
+        }
+    }
+}
+
+#pragma mark - MFMailComposeViewControllerDelegate
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller
+          didFinishWithResult:(MFMailComposeResult)result
+                        error:(NSError *)error {
+    switch (result) {
+        case MFMailComposeResultSent:
+            NSLog(@"邮件已发送");
+            break;
+        case MFMailComposeResultSaved:
+            NSLog(@"邮件已保存");
+            break;
+        case MFMailComposeResultCancelled:
+            NSLog(@"邮件已取消");
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"邮件发送失败: %@", error.localizedDescription);
+            break;
+        default:
+            break;
+    }
+
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 #pragma mark - UITableViewDelegate
