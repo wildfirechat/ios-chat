4948b57a9bbe014734688687c9143111b371612a
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2026-01-05 20:25:50 +0800

    文件消息识别电话号码/网址/邮箱并处理点击事件

--- wfuikit/WFChatUIKit/MessageList/Cell/WFCUTextCell.m
+++ wfuikit/WFChatUIKit/MessageList/Cell/WFCUTextCell.m
@@ -11,11 +11,12 @@
 #import "WFCUUtilities.h"
 #import "SelectableTextView.h"
 #import "WFCUConfigManager.h"
+#import <MessageUI/MessageUI.h>
 
 #define TEXT_LABEL_TOP_PADDING 3
 #define TEXT_LABEL_BUTTOM_PADDING 5
 
-@interface WFCUTextCell () <SelectableTextViewDelegate>
+@interface WFCUTextCell () <SelectableTextViewDelegate, MFMailComposeViewControllerDelegate>
 
 @end
 
@@ -81,11 +82,100 @@
 - (void)didSelectUrl:(NSString *)urlString {
     [self.delegate didSelectUrl:self withModel:self.model withUrl:urlString];
 }
+
 - (void)didSelectPhoneNumber:(NSString *)phoneNumberString {
     [self.delegate didSelectPhoneNumber:self withModel:self.model withPhoneNumber:phoneNumberString];
 }
+
+- (void)didSelectEmail:(NSString *)emailString {
+    [self sendEmailTo:emailString];
+}
+
 - (void)didLongPressTextView:(SelectableTextView *)textView {
     // 当 SelectableTextView 被长按时，触发 cell 的长按事件
     [self.delegate didLongPressMessageCell:self withModel:self.model];
 }
+
+// 发送邮件
+- (void)sendEmailTo:(NSString *)email {
+    if (!email || email.length == 0) {
+        return;
+    }
+
+    // 获取当前视图控制器
+    UIViewController *topVC = [self topViewController];
+    if (!topVC) {
+        return;
+    }
+
+    // 检查设备是否支持发送邮件
+    if ([MFMailComposeViewController canSendMail]) {
+        // 使用 MFMailComposeViewController
+        MFMailComposeViewController *mailComposeVC = [[MFMailComposeViewController alloc] init];
+        mailComposeVC.mailComposeDelegate = self;
+        [mailComposeVC setToRecipients:@[email]];
+
+        [topVC presentViewController:mailComposeVC animated:YES completion:nil];
+    } else {
+        // 如果不支持，使用 mailto: URL scheme
+        NSString *emailString = [NSString stringWithFormat:@"mailto:%@", email];
+        NSURL *emailURL = [NSURL URLWithString:emailString];
+
+        if ([[UIApplication sharedApplication] canOpenURL:emailURL]) {
+            [[UIApplication sharedApplication] openURL:emailURL options:@{} completionHandler:nil];
+        } else {
+            // 如果都无法发送，显示提示
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示"
+                                                                           message:@"您的设备不支持发送邮件"
+                                                                    preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:nil];
+            [alert addAction:okAction];
+            [topVC presentViewController:alert animated:YES completion:nil];
+        }
+    }
+}
+
+// 获取当前最顶层的视图控制器
+- (UIViewController *)topViewController {
+    UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
+    UIViewController *rootVC = keyWindow.rootViewController;
+
+    while (rootVC.presentedViewController) {
+        rootVC = rootVC.presentedViewController;
+    }
+
+    if ([rootVC isKindOfClass:[UINavigationController class]]) {
+        UINavigationController *nav = (UINavigationController *)rootVC;
+        return nav.visibleViewController;
+    }
+
+    return rootVC;
+}
+
+#pragma mark - MFMailComposeViewControllerDelegate
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller
+          didFinishWithResult:(MFMailComposeResult)result
+                        error:(NSError *)error {
+    switch (result) {
+        case MFMailComposeResultSent:
+            NSLog(@"邮件已发送");
+            break;
+        case MFMailComposeResultSaved:
+            NSLog(@"邮件已保存");
+            break;
+        case MFMailComposeResultCancelled:
+            NSLog(@"邮件已取消");
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"邮件发送失败: %@", error.localizedDescription);
+            break;
+        default:
+            break;
+    }
+
+    UIViewController *topVC = [self topViewController];
+    [topVC dismissViewControllerAnimated:YES completion:nil];
+}
+
 @end
